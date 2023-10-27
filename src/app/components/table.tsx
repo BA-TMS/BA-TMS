1683e3af61075b0d690e87e80f713af70eff341b
Lane Hamilton <aleyna.hamilton@gmail.com>
2023-10-27 14:46:48 -0700

    Table stying

--- src/app/components/table.tsx
+++ src/app/components/table.tsx
@@ -1,29 +1,50 @@
 // table to display form data
 // if i had more time, this could be a modal that pops up
 // also would make new rows be added to table each time form is filled out
+// table needs to be more rounded to fit with design aesthetic
 
 import { TableProps } from '@/types';
 
 export default function Table(props: TableProps) {
   return (
-    <div>
-      <table>
+    <div className="mt-4">
+      <table className="table-auto">
         <thead>
           <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>City</th>
-            <th>State</th>
-            <th>Zip</th>
+            <th className="border rounded px-4 py-2 uppercase tracking-wide text-white-700 text-xs font-bold mb-2">
+              First Name
+            </th>
+            <th className="border rounded px-4 py-2 uppercase tracking-wide text-white-700 text-xs font-bold mb-2">
+              Last Name
+            </th>
+            <th className="border rounded px-4 py-2 uppercase tracking-wide text-white-700 text-xs font-bold mb-2">
+              City
+            </th>
+            <th className="border rounded px-4 py-2 uppercase tracking-wide text-white-700 text-xs font-bold mb-2">
+              State
+            </th>
+            <th className="border rounded px-4 py-2 uppercase tracking-wide text-white-700 text-xs font-bold mb-2">
+              Zip
+            </th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td>{props.formData.firstName}</td>
-            <td>{props.formData.lastName}</td>
-            <td>{props.formData.city}</td>
-            <td>{props.formData.state}</td>
-            <td>{props.formData.zip}</td>
+            <td className="border rounded px-4 py-2 tracking-wide text-white-700 text-xs font-bold mb-2">
+              {props.formData.firstName}
+            </td>
+            <td className="border rounded px-4 py-2 tracking-wide text-white-700 text-xs font-bold mb-2">
+              {props.formData.lastName}
+            </td>
+            <td className="border rounded px-4 py-2 tracking-wide text-white-700 text-xs font-bold mb-2">
+              {props.formData.city}
+            </td>
+            <td className="border rounded px-4 py-2 tracking-wide text-white-700 text-xs font-bold mb-2">
+              {props.formData.state}
+            </td>
+            <td className="border rounded px-4 py-2 tracking-wide text-white-700 text-xs font-bold mb-2">
+              {props.formData.zip}
+            </td>
           </tr>
         </tbody>
       </table>
