18da6552b2857d95165bf9869234d17fc3388b2e
Lane Hamilton <aleyna.hamilton@gmail.com>
2023-10-27 16:45:22 -0700

    h1 color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,23 +13,21 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
-      extend: {
-        colors: {
-          primary: {
-            100: '#d1e9ff',
-            200: '#b2ddff',
-            300: '#84caff',
-            400: '#53b1fd',
-            500: '#2e90fa',
-            600: '#1570ef',
-            700: '#175cd3',
-            800: '#1849a9',
-            900: '#194185',
-          },
+
+      colors: {
+        primary: {
+          100: '#d1e9ff',
+          200: '#b2ddff',
+          300: '#84caff',
+          400: '#53b1fd',
+          500: '#2e90fa',
+          600: '#1570ef',
+          700: '#175cd3',
+          800: '#1849a9',
+          900: '#194185',
         },
       },
     },
-
     fontFamily: {
       sans: ['Helvetica', 'sans-serif'],
     },
