cebd163393f8d3d4d72fd9abca6f16521439fc0e
Lane Hamilton <aleyna.hamilton@gmail.com>
2023-10-27 17:31:57 -0700

    centered items in flex container

--- src/app/components/form.tsx
+++ src/app/components/form.tsx
@@ -119,8 +119,8 @@ export default function Form() {
   }
 
   return (
-    <div className="flex flex-col">
-      <form className="w-full max-w-lg">
+    <div className="flex flex-col content-center items-center ">
+      <form className="w-full max-w-lg ">
         <div className="flex flex-wrap -mx-3 mb-3">
           <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
             <label
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Form from './components/form';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen max-h-screen flex-col items-center justify-evenly p-24">
       <div className="relative flex place-items-center">
         <h1 className="text-6xl text-primary-800 dark:text-primary-300">
           A2ZPort
