70ddeb6d534a660138aa3de5f348326d5413eafc
Lane Hamilton <aleyna.hamilton@gmail.com>
2023-11-06 19:59:53 -0800

    get request to rick and morty api on backend

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -1,81 +1,88 @@
 import { NextResponse } from 'next/server';
 
-type Todo = {
-  userId: number;
+type Character = {
   id: number;
-  title: string;
-  completed: boolean;
+  name: string;
+  status: string;
+  species: string;
 };
 
 // placeholder API for testing out that I know how to make routes
-const DATA_SOURCE_URL: string = 'https://jsonplaceholder.typicode.com/todos';
+// move to .env and make it a database connection instead
+const DATA_SOURCE_URL: string = 'https://rickandmortyapi.com/api/character';
 const API_KEY: string = process.env.DATA_API_KEY as string;
 
 export async function GET() {
   const res = await fetch(DATA_SOURCE_URL);
 
-  const todos: Todo[] = await res.json();
+  const response = await res.json();
 
-  return NextResponse.json(todos);
-}
-
-export async function DELETE(request: Request) {
-  const { id }: Partial<Todo> = await request.json();
-
-  if (!id) return NextResponse.json({ message: 'Todo id required' });
-  await fetch(`${DATA_SOURCE_URL}/${id}`, {
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json()',
-      'API-Key': API_KEY,
-    },
+  // destructure to return only needed info
+  const filteredResults = response.results.map((item: Character) => {
+    const { name, id, species, status } = item;
+    return { name, id, species, status };
   });
-  return NextResponse.json({ message: `Todo ${id} deleted` });
+
+  return NextResponse.json(filteredResults);
 }
 
-// not sure if response is returning everything
-// could have something to do with the API
-export async function POST(request: Request) {
-  const { userId, title }: Partial<Todo> = await request.json();
+// export async function DELETE(request: Request) {
+//   const { id }: Partial<Todo> = await request.json();
 
-  if (!userId || !title)
-    return NextResponse.json({
-      message: 'Missing required data in post request',
-    });
+//   if (!id) return NextResponse.json({ message: 'Todo id required' });
+//   await fetch(`${DATA_SOURCE_URL}/${id}`, {
+//     method: 'DELETE',
+//     headers: {
+//       'Content-Type': 'application/json()',
+//       'API-Key': API_KEY,
+//     },
+//   });
+//   return NextResponse.json({ message: `Todo ${id} deleted` });
+// }
 
-  const res = await fetch(DATA_SOURCE_URL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json()',
-      'API-Key': API_KEY,
-    },
-    body: JSON.stringify({ userId, title, completed: false }),
-  });
-  const newTodo: Todo = await res.json();
-  console.log(newTodo);
-  return NextResponse.json(newTodo);
-  // returns only the id???
-}
+// // not sure if response is returning everything
+// // could have something to do with the API
+// export async function POST(request: Request) {
+//   const { userId, title }: Partial<Todo> = await request.json();
 
-export async function PUT(request: Request) {
-  const { userId, id, title, completed }: Todo = await request.json();
+//   if (!userId || !title)
+//     return NextResponse.json({
+//       message: 'Missing required data in post request',
+//     });
 
-  // if we do not have these things...
-  if (!userId || !id || !title || typeof completed !== 'boolean')
-    return NextResponse.json({
-      message: 'Missing required data in put request',
-    });
+//   const res = await fetch(DATA_SOURCE_URL, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json()',
+//       'API-Key': API_KEY,
+//     },
+//     body: JSON.stringify({ userId, title, completed: false }),
+//   });
+//   const newTodo: Todo = await res.json();
+//   console.log(newTodo);
+//   return NextResponse.json(newTodo);
+//   // returns only the id???
+// }
 
-  const res = await fetch(`${DATA_SOURCE_URL}/${id}`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json()',
-      'API-Key': API_KEY,
-    },
-    body: JSON.stringify({ userId, title, completed }),
-  });
-  const updatedTodo: Todo = await res.json();
-  console.log(updatedTodo);
-  return NextResponse.json(updatedTodo);
-  // also just returns the id apparently
-}
+// export async function PUT(request: Request) {
+//   const { userId, id, title, completed }: Todo = await request.json();
+
+//   // if we do not have these things...
+//   if (!userId || !id || !title || typeof completed !== 'boolean')
+//     return NextResponse.json({
+//       message: 'Missing required data in put request',
+//     });
+
+//   const res = await fetch(`${DATA_SOURCE_URL}/${id}`, {
+//     method: 'PUT',
+//     headers: {
+//       'Content-Type': 'application/json()',
+//       'API-Key': API_KEY,
+//     },
+//     body: JSON.stringify({ userId, title, completed }),
+//   });
+//   const updatedTodo: Todo = await res.json();
+//   console.log(updatedTodo);
+//   return NextResponse.json(updatedTodo);
+//   // also just returns the id apparently
+// }
