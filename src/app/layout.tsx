b73eaebab23c98cf35856b68f70106856e05f28d
SAOGonza <samuelortega86@yahoo.com>
2023-11-08 12:51:34 -0800

    Forgot to also save changes to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import AppProvider from './AppProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <AppProvider><body>{children}</body></AppProvider>
+      
     </html>
   )
 }
