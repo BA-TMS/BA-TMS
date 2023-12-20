14c93c5f69dd572fbfdb371757208dccc3635920
SAOGonza <samuelortega86@yahoo.com>
2023-12-19 19:35:56 -0800

    Added containers page
    TODO: Maybe add more elements to the web page.

---
+++ src/app/portal/containers/page.tsx
@@ -0,0 +1,12 @@
+import PortalLayout from '../../components/layouts/portal/PortalLayout';
+import { ReactNode } from 'react';
+
+const Containers: any = () => {
+  return (
+    <PortalLayout pageTitle="Containers">
+      <h1>Welcome to the Containers screen</h1>
+    </PortalLayout>
+  );
+};
+
+export default Containers;
