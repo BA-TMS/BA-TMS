842158c5bd809ccd7e51ff695c495b05a638b560
Lane Hamilton <aleyna.hamilton@gmail.com>
2023-12-21 14:05:59 -0800

    WIP with sample MUI table

--- src/app/portal/empty/page.tsx
+++ src/app/portal/empty/page.tsx
@@ -3,11 +3,13 @@
 import PortalLayout from '../../components/portal/PortalLayout';
 import Searchbar from '@/app/components/portal/header/Searchbar';
 import Header from '@/app/components/portal/header/Header';
+import TableTest from '@/app/components/table/table';
 
 const EmptyRecieving: any = () => {
   return (
     <PortalLayout pageTitle="Empty_Recieving">
       <h1>Empty Recieving</h1>
+      <TableTest />
     </PortalLayout>
   );
 };
