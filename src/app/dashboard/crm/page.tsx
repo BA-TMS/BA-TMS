511e9c8f9405e3a5445d0a1a376a7c0fdadfbbcd
Jacob Reola <jacobreola@gmail.com>
2024-01-10 09:57:23 -0600

    testing spacing and page

---
+++ src/app/dashboard/crm/page.tsx
@@ -0,0 +1,19 @@
+import Sidebar from "@/components/Sidebar";
+import Header from "@/components/Header";
+
+
+const CrmPage = () => {
+  return (
+    <>
+      {/* <Sidebar />
+      <Header /> */}
+      {Array(100).fill("Overflowing string ").map((str, index) => (
+        <p key={index} style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>
+          {str.repeat(100)}
+        </p>
+      ))}
+    </>
+  );
+};
+
+export default CrmPage;
