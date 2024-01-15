7cd063eeb7f4925606533d1fbe373a962cc37b25
Joaquin Maguire <thejmags@gmail.com>
2024-01-15 11:42:26 -0800

    Formatting for the seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
 // import { PrismaClient } from '@prisma/client';
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function main() {
@@ -12,9 +12,9 @@ async function main() {
 
   for (const currOrg of orgs) {
     const resp = await prisma.organization.upsert({
-      where: {name: currOrg.name},
+      where: { name: currOrg.name },
       update: {},
-      create: currOrg
+      create: currOrg,
     });
     orgIds.push(resp.id);
     // console.log(resp);
@@ -22,9 +22,9 @@ async function main() {
 
   for (const currCarrier of carriers) {
     const resp = await prisma.carrier.upsert({
-      where: {name: currCarrier.name},
+      where: { name: currCarrier.name },
       update: {},
-      create: currCarrier
+      create: currCarrier,
     });
     carrierIds.push(resp.id);
     // console.log(resp);
@@ -32,9 +32,9 @@ async function main() {
 
   for (const currCustomer of customers) {
     const resp = await prisma.customer.upsert({
-      where: {name: currCustomer.name},
+      where: { name: currCustomer.name },
       update: {},
-      create: currCustomer
+      create: currCustomer,
     });
     customerIds.push(resp.id);
     // console.log(resp);
@@ -44,14 +44,17 @@ async function main() {
     currLoad.ownerId = orgIds[loadPos % orgIds.length];
     currLoad.carrierId = carrierIds[loadPos % carrierIds.length];
     currLoad.customerId = customerIds[loadPos % customerIds.length];
-    const resp  = await prisma.load.upsert({
-      where: { ownerId_loadNum:
-        {ownerId: currLoad.ownerId, loadNum: currLoad.loadNum} 
+    const resp = await prisma.load.upsert({
+      where: {
+        ownerId_loadNum: {
+          ownerId: currLoad.ownerId,
+          loadNum: currLoad.loadNum,
+        },
       },
       update: {},
-      create: currLoad
+      create: currLoad,
     });
-    console.log(resp)
+    console.log(resp);
     loadPos += 1;
   }
 
@@ -62,169 +65,169 @@ async function main() {
 
 const orgs = [
   {
-    name: 'Org1',
-    address: '73 N. Hightland Court',
-    city: 'Lititz',
-    state: 'PA',
-    postCountry: 'USA',
-    postCode: '17543',
+    name: "Org1",
+    address: "73 N. Hightland Court",
+    city: "Lititz",
+    state: "PA",
+    postCountry: "USA",
+    postCode: "17543",
     telCountry: 1,
-    telephone: '1234567890'
+    telephone: "1234567890",
   },
   {
-    name: 'Org2',
-    address: '26 Mill Circle',
-    city: 'Greenwood',
-    state: 'SC',
-    postCountry: 'USA',
-    postCode: '17543',
+    name: "Org2",
+    address: "26 Mill Circle",
+    city: "Greenwood",
+    state: "SC",
+    postCountry: "USA",
+    postCode: "17543",
     telCountry: 1,
-    telephone: '6168675309'
-  }
-]
+    telephone: "6168675309",
+  },
+];
 
 const users = [
   {
-    email: 'testuser1@org1.com',
-    password: 'test1234',
+    email: "testuser1@org1.com",
+    password: "test1234",
     orgId: null,
-    role: 1
+    role: 1,
   },
   {
-    email: 'testuser2@org2.com',
-    password: 'test1234',
+    email: "testuser2@org2.com",
+    password: "test1234",
     orgId: null,
-    role: 1
-  }
-]
+    role: 1,
+  },
+];
 
 const carriers = [
   {
-    name: 'Carrier1',
-    address: '70 Wall St',
-    city: 'Wethersfield',
-    state: 'CT',
-    postCountry: 'USA',
-    postCode: '06109',
+    name: "Carrier1",
+    address: "70 Wall St",
+    city: "Wethersfield",
+    state: "CT",
+    postCountry: "USA",
+    postCode: "06109",
     telCountry: 1,
-    telephone: '9987654321',
-    dotId: 42
+    telephone: "9987654321",
+    dotId: 42,
   },
   {
-    name: 'Carrier2',
-    address: '79 Mayflower St',
-    city: 'Smyrna',
-    state: 'GA',
-    postCountry: 'USA',
-    postCode: '30080',
+    name: "Carrier2",
+    address: "79 Mayflower St",
+    city: "Smyrna",
+    state: "GA",
+    postCountry: "USA",
+    postCode: "30080",
     telCountry: 1,
-    telephone: '1112223333',
-    dotId: 7
-  }
-]
+    telephone: "1112223333",
+    dotId: 7,
+  },
+];
 
 const customers = [
   {
-    name: 'Customer1',
-    address: '26 Grand St',
-    city: 'Oxford',
-    state: 'MS',
-    postCountry: 'USA',
-    postCode: '38655',
+    name: "Customer1",
+    address: "26 Grand St",
+    city: "Oxford",
+    state: "MS",
+    postCountry: "USA",
+    postCode: "38655",
     telCountry: 1,
-    telephone: '4445556666'
+    telephone: "4445556666",
   },
   {
-    name: 'Customer2',
-    address: '54 Woodland St',
-    city: 'Nashua',
-    state: 'NH',
-    postCountry: 'USA',
-    postCode: '03060',
+    name: "Customer2",
+    address: "54 Woodland St",
+    city: "Nashua",
+    state: "NH",
+    postCountry: "USA",
+    postCode: "03060",
     telCountry: 1,
-    telephone: '7778889999'
-  }
-]
+    telephone: "7778889999",
+  },
+];
 
 const shippers = [
   {
-    name: 'Reylo',
-    address: '73 Newbridge St',
-    city: 'West Des Moines',
-    state: 'IA',
-    postCountry: 'USA',
-    postCode: '50265',
+    name: "Reylo",
+    address: "73 Newbridge St",
+    city: "West Des Moines",
+    state: "IA",
+    postCountry: "USA",
+    postCode: "50265",
     telCountry: 1,
-    telephone: '9998887777'
+    telephone: "9998887777",
   },
   {
-    name: 'Johnlock',
-    address: '2 Lookout Dr',
-    city: 'Perrysburg',
-    state: 'OH',
-    postCountry: 'USA',
-    postCode: '43551',
+    name: "Johnlock",
+    address: "2 Lookout Dr",
+    city: "Perrysburg",
+    state: "OH",
+    postCountry: "USA",
+    postCode: "43551",
     telCountry: 1,
-    telephone: '6665554444'
-  }
-]
+    telephone: "6665554444",
+  },
+];
 
 const consignees = [
   {
-    name: 'Consignee1',
-    address: '7524 Pine Road',
-    city: 'Daphne',
-    state: 'AL',
-    postCountry: 'USA',
-    postCode: '36526',
+    name: "Consignee1",
+    address: "7524 Pine Road",
+    city: "Daphne",
+    state: "AL",
+    postCountry: "USA",
+    postCode: "36526",
     telCountry: 1,
-    telephone: '2923834774'
+    telephone: "2923834774",
   },
   {
-    name: 'Consignee2',
-    address: '7352 Rosewood Street',
-    city: 'Deerfield',
-    state: 'IL',
-    postCountry: 'USA',
-    postCode: '60015',
+    name: "Consignee2",
+    address: "7352 Rosewood Street",
+    city: "Deerfield",
+    state: "IL",
+    postCountry: "USA",
+    postCode: "60015",
     telCountry: 1,
-    telephone: '9298387447'
-  }
-]
+    telephone: "9298387447",
+  },
+];
 
 const loads = [
   {
     ownerId: null,
     loadNum: 69,
     carrierId: null,
-    customerId: null
+    customerId: null,
   },
   {
     ownerId: null,
     loadNum: 420,
     carrierId: null,
-    customerId: null
+    customerId: null,
   },
   {
     ownerId: null,
     loadNum: 1111,
     carrierId: null,
-    customerId: null
+    customerId: null,
   },
   {
     ownerId: null,
     loadNum: 2222,
     carrierId: null,
-    customerId: null
-  }
-]
+    customerId: null,
+  },
+];
 
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
   });
