7bddbe17d272c755c3d94fe5ba852ed4e796df09
Jacob Reola <jacobreola@gmail.com>
2024-01-16 08:29:21 -0600

    dashboard foundation set

--- src/app/dashboard/crm/page.tsx
+++ src/app/dashboard/crm/page.tsx
@@ -1,12 +1,14 @@
 import Sidebar from '@/components/Sidebar';
 import Header from '@/components/Header/Header';
+import CRM from "@/components/Dashboard/CRM";
+
 
 const CrmPage = () => {
   return (
     <>
       {/* <Sidebar />
       <Header /> */}
-      {Array(100)
+      {/* {Array(100)
         .fill('Overflowing string ')
         .map((str, index) => (
           <p
@@ -19,7 +21,8 @@ const CrmPage = () => {
           >
             {str.repeat(100)}
           </p>
-        ))}
+        ))} */}
+        <CRM />
     </>
   );
 };
