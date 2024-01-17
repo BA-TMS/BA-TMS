c245c9e5772fca39d329bc6cf7d01133b49ee210
SAOGonza <samuelortega86@yahoo.com>
2024-01-17 15:03:25 -0800

    Added tabs
    TODO: Needs functionality for change-password and Team.

---
+++ src/app/user/settings/change-password/page.tsx
@@ -0,0 +1,50 @@
+'use client'
+import { ReactNode, useState } from 'react';
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+
+// TESTING
+import Input from '../../../../components/Settings-General/Input';
+import Form, { type FormHandle } from '../../../../components/Settings-General/Form';
+import Button from '../../../../components/Settings-General/Button';
+import { useRef } from 'react';
+
+
+
+const SettingsPage = () => {
+    const customers = [
+        { code: 'string', address: 'string', city: 'string', zip: 'string', state: 'string', phoneNumber: "string" }
+    ];
+
+    // Need a ref that we connect to our custom component.
+    const customForm = useRef<FormHandle>(null);
+
+    function handleSave(data: unknown) {
+        // Convince TypeScript that we know what the data is.
+        const extractedData = data as {
+          code: string;
+          address: string;
+          country: string;
+          state: string;
+          city: string;
+          zip: string;
+          name: string;
+          email:string;
+          phoneNumber: string
+        };
+        console.log(extractedData);
+        customForm.current?.clear(); // Current could be null, so add a ?.
+    
+        // Add new ID to the new customer and put them at the end of the list.
+        // First, convert the last value of 'code' into an int, add 1, then turn it
+        // back into a string, then set that as the new ID for the newest customer.
+        extractedData.code = String(parseInt(customers[customers.length - 1].code) + 1);
+        customers.push(extractedData);
+      }
+
+
+    return (
+        <h1>Change Password Here!</h1>
+    );
+};
+
+export default SettingsPage;
---
+++ src/app/user/settings/layout.tsx
@@ -0,0 +1,65 @@
+'use client';
+import { Container, Tab, Tabs, Box, BoxProps } from '@mui/material';
+import { useState } from 'react';
+import SettingsPage from './general/page';
+import Link from 'next/link';
+
+import { forwardRef } from 'react';
+// icons
+import { Icon } from '@iconify/react';
+//
+import { IconifyProps } from './types';
+
+// ----------------------------------------------------------------------
+
+interface Props extends BoxProps {
+  icon: IconifyProps;
+}
+
+const Iconify = forwardRef<SVGElement, Props>(({ icon, width = 20, sx, ...other }, ref) => (
+  <Box ref={ref} component={Icon} icon={icon} sx={{ width, height: width, ...sx }} {...other} />
+));
+
+/*type Labels = {
+    general?: string;
+    change_password?: string;
+    team?: string;
+}
+
+const useGeneralLabels = () => {
+    // Fix next hydration.
+    const { translate } = useLocales();
+}*/
+
+export default function UserSettingsLayout({
+    children,
+  }: {
+    children: React.ReactNode;
+  }) {
+    // const [sidebarOpen, setSidebarOpen] = useState(false);
+  
+    // const [loading, setLoading] = useState<boolean>(true);
+  
+    // useEffect(() => {
+    //   setTimeout(() => setLoading(false), 1000);
+    // }, []);
+
+    return (
+      <Container disableGutters sx={{padding: 0, margin: 0}}>
+        <Tabs>
+          <Link href="/user/settings/general">
+            <Tab sx={{'color': 'white', padding: 2, minHeight: "unset"}} label="general" iconPosition="start" icon={<Iconify icon="ic:round-account-box" />} />
+          </Link>
+          
+          <Link href="/user/settings/change-password">
+            <Tab sx={{'color': 'white', padding: 2, marginLeft: 5, minHeight: "unset"}} label="change-password" iconPosition="start" icon={<Iconify icon="ic:round-vpn-key" />} />
+          </Link>
+          
+          <Link href="/user/settings/team">
+            <Tab sx={{'color': 'white', padding: 2, marginLeft: 5, minHeight: "unset"}} label="team" iconPosition="start" icon={<Iconify icon="ic:round-vpn-key"/>} />
+          </Link>
+        </Tabs>
+        {children}
+      </Container>
+    );
+  }
