7eadda5b1333765088d42d7b7cdc87ef0554f25b
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-01-18 12:38:10 -0800

    tailwind config updates from Tailadin template + minor fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import CRM from '@/components/Dashboard/CRM';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'TailAdmin | E-commerce Dashboard Tempalte',
+  title: 'TailAdmin | E-commerce Dashboard Template',
   description: 'This is Home for TailAdmin',
   // other metadata
 };
