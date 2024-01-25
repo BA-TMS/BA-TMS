73bf1ad4617e0ace969a78c48a95887338068004
SAOGonza <samuelortega86@yahoo.com>
2024-01-25 13:19:34 -0800

    Added Jacob's changes
    TODO: Fix general tabs up top to align with general info.
    
    - Get rid of material UI on password.

--- src/app/user/settings/layout.tsx
+++ src/app/user/settings/layout.tsx
@@ -4,11 +4,9 @@ import { useState } from 'react';
 import SettingsPage from './general/page';
 import Link from 'next/link';
 
-
 // icons
 import Iconify from '@/components/iconify/Iconify';
 //
-
 
 // ----------------------------------------------------------------------
 
@@ -24,34 +22,59 @@ const useGeneralLabels = () => {
 }*/
 
 export default function UserSettingsLayout({
-    children,
-  }: {
-    children: React.ReactNode;
-  }) {
-    // const [sidebarOpen, setSidebarOpen] = useState(false);
-  
-    // const [loading, setLoading] = useState<boolean>(true);
-  
-    // useEffect(() => {
-    //   setTimeout(() => setLoading(false), 1000);
-    // }, []);
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  // const [sidebarOpen, setSidebarOpen] = useState(false);
 
-    return (
-      <Container disableGutters sx={{padding: 0, margin: 0}}>
-        <Tabs>
-          <Link href="/user/settings/general">
-            <Tab sx={{'color': 'white', padding: 2, minHeight: "unset"}} label="general" iconPosition="start" icon={<Iconify icon="ic:round-account-box" />} />
-          </Link>
-          
-          <Link href="/user/settings/change-password">
-            <Tab sx={{'color': 'white', padding: 2, marginLeft: 5, minHeight: "unset"}} label="change-password" iconPosition="start" icon={<Iconify icon="ic:round-vpn-key" />} />
-          </Link>
-          
-          <Link href="/user/settings/team">
-            <Tab sx={{'color': 'white', padding: 2, marginLeft: 5, minHeight: "unset"}} label="team" iconPosition="start" icon={<Iconify icon="ic:round-vpn-key"/>} />
-          </Link>
-        </Tabs>
-        {children}
-      </Container>
-    );
-  }
+  // const [loading, setLoading] = useState<boolean>(true);
+
+  // useEffect(() => {
+  //   setTimeout(() => setLoading(false), 1000);
+  // }, []);
+
+  return (
+    <Container disableGutters sx={{ padding: 0, margin: 0 }}>
+      <Tabs>
+        <Link href="/user/settings/general">
+          <Tab
+            sx={{ color: 'white', padding: 2, minHeight: 'unset' }}
+            label="general"
+            iconPosition="start"
+            icon={<Iconify icon="ic:round-account-box" />}
+          />
+        </Link>
+
+        <Link href="/user/settings/change-password">
+          <Tab
+            sx={{
+              color: 'white',
+              padding: 2,
+              marginLeft: 5,
+              minHeight: 'unset',
+            }}
+            label="change-password"
+            iconPosition="start"
+            icon={<Iconify icon="ic:round-vpn-key" />}
+          />
+        </Link>
+
+        <Link href="/user/settings/team">
+          <Tab
+            sx={{
+              color: 'white',
+              padding: 2,
+              marginLeft: 5,
+              minHeight: 'unset',
+            }}
+            label="team"
+            iconPosition="start"
+            icon={<Iconify icon="ic:round-vpn-key" />}
+          />
+        </Link>
+      </Tabs>
+      {children}
+    </Container>
+  );
+}
