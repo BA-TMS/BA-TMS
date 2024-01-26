b6bd2fe57abb3a601eba31189d3da252a97dec44
Jacob Reola <jacobreola@gmail.com>
2024-01-26 02:51:36 -0600

    init new table no functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,10 @@
 
 import ModalOne from "@/components/Modals/ModalOne";
 import DataTableTwo from "@/components/DataTables/DataTableTwo";
+import TableThree from "@/components/Table/TableThree";
+import TableJ from "@/components/Table/TableJ";
+
+
 import { useState } from 'react';
 
 // Place the modal as an add button to the right side of the screen.
@@ -16,7 +20,9 @@ export default function Home() {
   
   return (
     <>
-    <DataTableTwo />
+    <TableJ />
+      {/* <TableThree/> */}
+      {/* <DataTableTwo /> */}
       {/* <ModalOne /> */}
     </>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -183,3 +183,4 @@ const config: Config = {
   plugins: [],
 };
 export default config;
+
