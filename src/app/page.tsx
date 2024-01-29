f184d3d0ff2112c6ab9150bad9108b18ccd699c7
Jacob Reola <jacobreola@gmail.com>
2024-01-29 02:22:13 -0600

    removed uneeded code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,10 @@
 'use client';
 
-
 import ModalOne from "@/components/Modals/ModalOne";
 import TableThree from "@/components/Table/TableThree";
 import TableJ from "@/components/Table/TableJ";
 
-
-import { useState } from 'react';
-
-// Place the modal as an add button to the right side of the screen.
-
-export default function Home() {
-  const [modalOpen, setModalOpen] = useState(false);
-  
-  const toggleModal = () => {
-    setModalOpen(!modalOpen);
-  };
-  
+export default function Home() {  
   return (
     <>
        <TableJ />
