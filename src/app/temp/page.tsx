b36abf5930dd70e81a1f313519156fe741ed7a91
Jacob Reola <jacobreola@gmail.com>
2024-02-06 13:25:45 -0600

    temporary tests

--- src/app/temp/page.tsx
+++ src/app/temp/page.tsx
@@ -5,11 +5,14 @@
 import ModalOne from "@/components/Modals/ModalOne";
 import TableThree from "@/components/Table/TableThree";
 import TableJ from "@/components/Table/TableJ";
+import Example from "@/components/Table/Example";
+
 
 export default function Home() {  
   return (
     <>
-       <TableJ />
+    <Example />
+      {/* <TableJ /> */}
       {/* <TableThree/> */}
       {/* <ModalOne /> */}
     </>
