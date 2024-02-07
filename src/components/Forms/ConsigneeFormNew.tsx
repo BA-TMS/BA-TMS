b1150b637925e07f063ef82bf0769a0ba11bda63
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-02-07 14:09:02 -0800

    Update: reusable Select component

--- src/components/Forms/ConsigneeFormNew.tsx
+++ src/components/Forms/ConsigneeFormNew.tsx
@@ -1,34 +1,40 @@
 'use client';
 
-import { useForm, type SubmitHandler } from 'react-hook-form';
+import {
+  useForm,
+  type SubmitHandler,
+  UseControllerProps,
+} from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 
 import TextInput from './TextInput';
+import SelectInput from './SelectInput';
+import { usStates } from '@/assets/data/states';
 
 const consigneeSchema = yup.object({
   'Consignee Name': yup.string().required('Consignee Name is required'),
   Address: yup.string().required('Address is required'),
-  // country: yup.string().required('Country is required'),
-  // state: yup.string().required('State is required '),
-  // city: yup.string().required('City is required '),
-  // zip: yup
-  //   .string()
-  //   .length(5, 'Zip must be 5 characters')
-  //   .required('Zip Code is required '),
-  // contactName: yup.string().required('Contact Name is required'),
-  // phone: yup
-  //   .string()
-  //   .matches(/^[0-9]+$/, 'Must use valid phone number')
-  //   .required('Contact phone number required'),
-  // email: yup
-  //   .string()
-  //   .email('Must use a valid email')
-  //   .required('Contact email required'),
-  // notes: yup.string().max(250, 'Must be under 250 characters'),
+  Country: yup.string().required('Country is required'),
+  State: yup.string().required('State is required '),
+  City: yup.string().required('City is required '),
+  Zip: yup
+    .string()
+    .length(5, 'Zip must be 5 characters')
+    .required('Zip Code is required '),
+  'Contact Name': yup.string().required('Contact Name is required'),
+  'Phone Number': yup
+    .string()
+    .matches(/^[0-9]+$/, 'Must use valid phone number')
+    .required('Contact phone number required'),
+  Email: yup
+    .string()
+    .email('Must use a valid email')
+    .required('Contact email required'),
+  Notes: yup.string().max(250, 'Must be under 250 characters'),
 });
 
-export type Consignee = yup.InferType<typeof consigneeSchema>;
+type Consignee = yup.InferType<typeof consigneeSchema>;
 
 export const FormComponent = (props: Consignee) => {
   const {
@@ -36,11 +42,19 @@ export const FormComponent = (props: Consignee) => {
     clearErrors, // will we need to clear errors?
     reset, // for resetting form
     control, // seems based on schema
-    formState: { errors, isSubmitting }, // errors for the errors in validation, isSubmitting is boolean when form is submitting
+    formState: { errors, isSubmitting }, // errors for root errors in validation, isSubmitting is boolean when form is submitting
   } = useForm<Consignee>({
     defaultValues: {
       'Consignee Name': '',
       Address: '',
+      Country: '',
+      State: '',
+      City: '',
+      Zip: '',
+      'Contact Name': '',
+      'Phone Number': '',
+      Email: '',
+      Notes: '',
     },
     resolver: yupResolver(consigneeSchema),
   });
@@ -57,6 +71,14 @@ export const FormComponent = (props: Consignee) => {
         name="Consignee Name" // needs to match schema
       />
       <TextInput control={control} name="Address" />
+      <TextInput control={control} name="City" />
+      <SelectInput control={control} name="State" options={usStates} />
+      <TextInput control={control} name="Zip" />
+      <TextInput control={control} name="Contact Name" />
+      <TextInput control={control} name="Phone Number" />
+      <TextInput control={control} name="Email" />
+      <TextInput control={control} name="Notes" isTextArea={true} />
+      {errors.root && <p className="mt-1 text-danger">{errors.root.message}</p>}
       <div className="flex justify-end">
         <button
           type="submit"
--- src/components/Forms/TextInput.tsx
+++ src/components/Forms/TextInput.tsx
@@ -20,12 +20,14 @@ const TextInput = (props: TextInputProps) => {
         <textarea
           {...field}
           rows={props.rows || 3}
+          placeholder={field.name}
           className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
         />
       ) : (
         <input
           {...field}
           type="text"
+          placeholder={field.name}
           className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
         />
       )}
