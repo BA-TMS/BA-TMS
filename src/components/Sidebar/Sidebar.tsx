4c3207f2cd36f7f12af2e62413e1b1df3163ac1f
Jacob Reola <jacobreola@gmail.com>
2024-02-13 10:50:29 -0600

    fixed temp route for consignee form and removed from /dashboard

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -320,6 +320,16 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                               Dispatch
                             </Link>
                           </li>
+                          <li>
+                            <Link
+                              href="/consignees"
+                              className={`group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white ${
+                                pathname === '/consignees' && 'text-white'
+                              } `}
+                            >
+                              Consignees
+                            </Link>
+                          </li>
                         </ul>
                       </div>
                       {/* <!-- Dropdown Menu End --> */}
