23a8a4689897427ab58adc0a27e5692824f63f4e
Jacob Reola <jacobreola@gmail.com>
2024-02-15 00:13:00 -0600

    init new file

---
+++ src/app/dashboard/group1/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TabGroupOne from '@/components/Dashboard/TabGroupOne';
+
+
+const GroupOne: React.FC = () => {
+  return (
+    <TabGroupOne />
+  );
+};
+
+export default GroupOne;
+
+
