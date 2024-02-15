1224e2e4c4ab0cdf2f61c69c970ad8fea927607e
Jacob Reola <jacobreola@gmail.com>
2024-02-15 00:40:14 -0600

    added add button

--- src/components/Dashboard/TabGroupOne.tsx
+++ src/components/Dashboard/TabGroupOne.tsx
@@ -24,68 +24,76 @@ const TabGroupOne: React.FC = () => {
   const inactiveClasses = "border-transparent";
 
   return (
-    <div className="rounded-sm border border-stroke bg-white p-7.5 shadow-default dark:border-strokedark dark:bg-boxdark">
-      <div className="mb-6 flex flex-wrap gap-5 border-b border-stroke dark:border-strokedark sm:gap-10">
-        <Link
-          href="#"
-          className={`border-b-2 py-4 text-sm font-medium hover:text-primary md:text-base ${
-            openTab === 1 ? activeClasses : inactiveClasses
-          }`}
-          onClick={() => setOpenTab(1)}
-        >
-          Customer
-        </Link>
-        <Link
-          href="#"
-          className={`border-b-2 py-4 text-sm font-medium hover:text-primary md:text-base ${
-            openTab === 2 ? activeClasses : inactiveClasses
-          }`}
-          onClick={() => setOpenTab(2)}
-        >
-          Consignee
-        </Link>
-        <Link
-          href="#"
-          className={`border-b-2 py-4 text-sm font-medium hover:text-primary md:text-base ${
-            openTab === 3 ? activeClasses : inactiveClasses
-          }`}
-          onClick={() => setOpenTab(3)}
-        >
-          Shipper
-        </Link>
+    <>
+      <div className="flex justify-end mb-4">
+        <button className="rounded-md bg-primary py-3 px-9 font-medium text-white">
+          Add
+        </button>
       </div>
-
-      <div>
-        {/* customer tab */}
-        <div
-          className={`leading-relaxed ${openTab === 1 ? "block" : "hidden"}`}
-        >
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          luctus ligula nec dolor placerat, a consequat elit volutpat. Quisque
-          nibh lacus, posuere et turpis in, pretium facilisis nisl. Proin congue
-          sem vel sollicitudin sagittis. Class aptent taciti sociosqu ad litora
-          torquent per conubia nostra, per
+      
+      <div className="rounded-sm border border-stroke bg-white p-7.5 shadow-default dark:border-strokedark dark:bg-boxdark">
+        <div className="mb-6 flex flex-wrap gap-5 border-b border-stroke dark:border-strokedark sm:gap-10">
+          <Link
+            href="#"
+            className={`border-b-2 py-4 text-sm font-medium hover:text-primary md:text-base ${
+              openTab === 1 ? activeClasses : inactiveClasses
+            }`}
+            onClick={() => setOpenTab(1)}
+          >
+            Customer
+          </Link>
+          <Link
+            href="#"
+            className={`border-b-2 py-4 text-sm font-medium hover:text-primary md:text-base ${
+              openTab === 2 ? activeClasses : inactiveClasses
+            }`}
+            onClick={() => setOpenTab(2)}
+          >
+            Consignee
+          </Link>
+          <Link
+            href="#"
+            className={`border-b-2 py-4 text-sm font-medium hover:text-primary md:text-base ${
+              openTab === 3 ? activeClasses : inactiveClasses
+            }`}
+            onClick={() => setOpenTab(3)}
+          >
+            Shipper
+          </Link>
         </div>
-        {/* consignee tab */}
-        <div
-          className={`leading-relaxed ${openTab === 2 ? "block" : "hidden"}`}
-        >
-          <ConsigneeTable data={data} />
-        </div>
-        {/* shipper tab */}
-        <div
-          className={`leading-relaxed ${openTab === 3 ? "block" : "hidden"}`}
-        >
-          Tab3 ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          luctus ligula nec dolor placerat, a consequat elit volutpat. Quisque
-          nibh lacus, posuere et turpis in, pretium facilisis nisl. Proin congue
-          sem vel sollicitudin sagittis. Class aptent taciti sociosqu ad litora
-          torquent per conubia nostra, per
+
+        <div>
+          {/* customer tab */}
+          <div
+            className={`leading-relaxed ${openTab === 1 ? "block" : "hidden"}`}
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            luctus ligula nec dolor placerat, a consequat elit volutpat. Quisque
+            nibh lacus, posuere et turpis in, pretium facilisis nisl. Proin congue
+            sem vel sollicitudin sagittis. Class aptent taciti sociosqu ad litora
+            torquent per conubia nostra, per
+          </div>
+          {/* consignee tab */}
+          <div
+            className={`leading-relaxed ${openTab === 2 ? "block" : "hidden"}`}
+          >
+            <ConsigneeTable data={data} />
+          </div>
+          {/* shipper tab */}
+          <div
+            className={`leading-relaxed ${openTab === 3 ? "block" : "hidden"}`}
+          >
+            Tab3 ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            luctus ligula nec dolor placerat, a consequat elit volutpat. Quisque
+            nibh lacus, posuere et turpis in, pretium facilisis nisl. Proin congue
+            sem vel sollicitudin sagittis. Class aptent taciti sociosqu ad litora
+            torquent per conubia nostra, per
+
+          </div>
 
         </div>
-
       </div>
-    </div>
+    </>
   );
 };
 
