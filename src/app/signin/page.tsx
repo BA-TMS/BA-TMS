4144854343db16ef709881f7d816ba02f0d96aa7
Jacob Reola <jacobreola@gmail.com>
2024-02-22 12:26:58 -0600

    init sign in form and created component for supabase

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,13 @@
+// For example purposes only to quickly test the componets
+
+'use client';
+
+import SignIn from '@/components/Authentication/SignIn';
+
+export default function Home() {
+  return (
+    <>
+      <SignIn />
+    </>
+  );
+}
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -330,6 +330,16 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                               Consignees
                             </Link>
                           </li>
+                          <li>
+                            <Link
+                              href="/signin"
+                              className={`group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white ${
+                                pathname === '/signin' && 'text-white'
+                              } `}
+                            >
+                              Sign In
+                            </Link>
+                          </li>
                         </ul>
                       </div>
                       {/* <!-- Dropdown Menu End --> */}
