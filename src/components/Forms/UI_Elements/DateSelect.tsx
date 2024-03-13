8463bdce3710325d5bba33857739d7a4419056f7
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-03-13 13:03:25 -0700

    Update style and add icon to DateSelect

--- src/components/Forms/UI_Elements/DateSelect.tsx
+++ src/components/Forms/UI_Elements/DateSelect.tsx
@@ -18,7 +18,7 @@ const DateSelect = (props: DateSelectProps) => {
         {props.required && <span className="text-danger"> *</span>}
         <input
           type="date"
-          className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary dark:white"
+          className="custom-input-date custom-input-date-1 w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary dark:white"
           {...field}
         />
         {fieldState.error && (
