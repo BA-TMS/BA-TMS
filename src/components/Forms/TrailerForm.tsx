dd20d2e68792a37f8e475868d69b379588bdc070
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-03-14 13:52:34 -0700

    trailer form update

--- src/components/Forms/TrailerForm.tsx
+++ src/components/Forms/TrailerForm.tsx
@@ -6,14 +6,17 @@ import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import TextInput from './UI_Elements/TextInput';
 import SelectInput from './UI_Elements/SelectInput';
-import { status } from './data/details';
+import DateSelect from './UI_Elements/DateSelect';
+import { status, trailers } from './data/details';
 import { ModalContext } from '@/Context/modalContext';
+import { addTrailer } from '@/lib/dbActions';
 
 const trailerSchema = yup.object({
-  'Trailer Number': yup.string().required('Trailer Number is required'),
   'Trailer Type': yup.string(),
-  'License Plate': yup.string(),
-  Status: yup.string(),
+  'License Plate': yup.string().required('License Plate is required'),
+  'Plate Expiry': yup.date().required('Plate Expiry is required'),
+  'Inspection Expiry': yup.date().required('Inspection Expiry is required'),
+  Status: yup.string().required('Status is required'),
   Notes: yup.string().max(250, 'Must be under 250 characters'),
 });
 
@@ -22,15 +25,16 @@ type Trailer = yup.InferType<typeof trailerSchema>;
 export const TrailerForm = () => {
   const {
     handleSubmit,
-    setError, // async error handling
-    reset, // for resetting form
-    control, // based on schema
-    formState: { errors, isSubmitting, isSubmitSuccessful }, // boolean values representing form state
+    setError,
+    reset,
+    control,
+    formState: { errors, isSubmitting, isSubmitSuccessful },
   } = useForm<Trailer>({
     defaultValues: {
-      'Trailer Number': '',
       'Trailer Type': '',
       'License Plate': '',
+      'Plate Expiry': new Date(),
+      'Inspection Expiry': new Date(),
       Status: '',
       Notes: '',
     },
@@ -42,8 +46,8 @@ export const TrailerForm = () => {
   const onSubmit = async (data: Trailer) => {
     console.log(data);
     try {
-      //   add database integration
-      console.log('External Carrier added successfully');
+      await addTrailer({ trailer: data });
+      console.log('Trailer added successfully');
       toggleOpen();
     } catch (error) {
       console.log('Error submitting form:', error);
@@ -54,9 +58,10 @@ export const TrailerForm = () => {
   useEffect(() => {
     if (isSubmitSuccessful) {
       reset({
-        'Trailer Number': '',
         'Trailer Type': '',
         'License Plate': '',
+        'Plate Expiry': new Date(),
+        'Inspection Expiry': new Date(),
         Status: '',
         Notes: '',
       });
@@ -73,15 +78,34 @@ export const TrailerForm = () => {
         </div>
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className="p-6.5">
-            <TextInput
-              control={control}
-              name="Trailer Number"
-              required={true}
-            />
-
-            <TextInput control={control} name="Trailer Type" />
-            <TextInput control={control} name="License Plate" />
-            <SelectInput control={control} name="Status" options={status} />
+            <TextInput control={control} name="License Plate" required={true} />
+            <div className=" flex flex-col gap-6 xl:flex-row">
+              <div className="w-full xl:w-1/2">
+                <DateSelect
+                  control={control}
+                  name="Plate Expiry"
+                  required={true}
+                />
+                <SelectInput
+                  control={control}
+                  options={trailers}
+                  name="Trailer Type"
+                />
+              </div>
+              <div className="w-full xl:w-1/2">
+                <DateSelect
+                  control={control}
+                  name="Inspection Expiry"
+                  required={true}
+                />
+                <SelectInput
+                  control={control}
+                  options={status}
+                  name="Status"
+                  required={true}
+                />
+              </div>
+            </div>
 
             <TextInput control={control} name="Notes" isTextArea={true} />
             {errors.root && (
