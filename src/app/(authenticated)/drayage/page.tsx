99e3afa67d28cf115ab73982cc7344b8fe542100
SAOGonza <samuelortega86@yahoo.com>
2024-03-28 11:50:49 -0700

    Added dummy data for dropdown.
    TODO: Add real options relating to TMS.

--- src/app/(authenticated)/drayage/page.tsx
+++ src/app/(authenticated)/drayage/page.tsx
@@ -1,8 +1,20 @@
+import SelectInput from '@/components/Forms/UI_Elements/SelectInput';
+import { usStates } from '@/components/Forms/data/states';
+
 export default function Drayage() {
   return (
     <div>
       <h1>Welcome to Drayage Page</h1>
       <p>This is a page for managing drayage operations.</p>
+      <div className="mb-4.5">
+        <label className="mb-2.5 block text-black dark:text-white">
+          <select className="relative z-20 w-full rounded border border-stroke bg-transparent py-3 px-5 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary">
+            <option value="apple">Stream 1</option>
+            <option value="banana">Stream 2</option>
+            <option value="orange">Stream 3</option>
+          </select>
+        </label>
+      </div>
     </div>
   );
 }
