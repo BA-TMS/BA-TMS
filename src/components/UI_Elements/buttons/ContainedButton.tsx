6c930fcacacd0eb668e4d5fad2564ee4db7a9b01
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-04-03 10:50:56 -0700

    Hover State for ContainedButton

--- src/components/UI_Elements/buttons/ContainedButton.tsx
+++ src/components/UI_Elements/buttons/ContainedButton.tsx
@@ -2,7 +2,7 @@ import { cva, VariantProps } from 'class-variance-authority';
 import { ButtonOrLink, Props as ButtonOrLinkProps } from './ButtonOrLink';
 
 /**
- * This is a base component that will render a button with specific styling depending on the props that are passed to it by using class-variance-authority.
+This is a base component that will render a button with specific styling depending on the props that are passed to it by using class-variance-authority. Must be used in a client component if passing an onClick function. This may need modified in the future
  */
 
 const buttonStyles = cva(
@@ -11,11 +11,15 @@ const buttonStyles = cva(
     variants: {
       intent: {
         default: 'bg-grey-300 text-black hover:shadow-default',
-        primary: 'bg-primary text-white hover:shadow-hover-primary',
-        info: 'bg-info text-white hover:shadow-hover-info',
-        success: 'bg-success text-white hover:shadow-hover-success',
-        warning: 'bg-warning text-black hover:shadow-hover-warning',
-        error: 'bg-error text-white hover:shadow-hover-error',
+        primary:
+          'bg-primary text-white hover:shadow-hover-primary hover:bg-primary-dark',
+        info: 'bg-info text-white hover:shadow-hover-info hover:bg-info-dark',
+        success:
+          'bg-success text-white hover:shadow-hover-success hover:bg-success-dark',
+        warning:
+          'bg-warning text-black hover:shadow-hover-warning hover:bg-warning-dark',
+        error:
+          'bg-error text-white hover:shadow-hover-error hover:bg-error-dark',
       },
       size: {
         large: 'px-5.5 py-2.75 text-button-lg',
