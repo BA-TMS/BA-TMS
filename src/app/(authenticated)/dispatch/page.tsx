256e2b1cccdf835e7e22281967ba5fea0b357dbf
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-04-09 10:08:59 -0700

    remove duplicate dispatch page

--- src/app/(authenticated)/dispatch/page.tsx
+++
@@ -1,8 +0,0 @@
-// import { ContextProvider } from '@/Context/modalContext';
-import Load from '@/components/Table/Load';
-
-export default async function DispatchPage() {
-  return (
-    <Load />
-  );
-}
