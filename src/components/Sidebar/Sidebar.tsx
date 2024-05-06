55dce341512e0359c01db5018678fd0a8d6c3332
SAOGonza <samuelortega86@yahoo.com>
2024-05-06 11:23:07 -0700

    Added 4 dropdown pages for Admin
    TODO: Make a working link to these pages. This can likely be done by re-routing the URLs

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -378,6 +378,141 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                 {SettingsIcon}
                 Preferences
               </Link>
+
+              {/* ADMIN DROPDOWN */}
+              <SidebarLinkGroup
+                activeCondition={
+                  pathname === '/pages' || pathname.includes('pages')
+                }
+              >
+                {(handleClick, open) => {
+                  return (
+                    <React.Fragment>
+                      <Link
+                        href="#"
+                        className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
+                          (pathname === '/pages' ||
+                            pathname.includes('pages')) &&
+                          'bg-graydark dark:bg-meta-4'
+                        }`}
+                        onClick={(e) => {
+                          e.preventDefault();
+                          sidebarExpanded
+                            ? handleClick()
+                            : setSidebarExpanded(true);
+                        }}
+                      >
+                        {SidebarUserIcon}
+                        Admin
+                        <svg
+                          className={`absolute right-4 top-1/2 -translate-y-1/2 fill-current ${
+                            open && 'rotate-180'
+                          }`}
+                          width="20"
+                          height="20"
+                          viewBox="0 0 20 20"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            clipRule="evenodd"
+                            d="M4.41107 6.9107C4.73651 6.58527 5.26414 6.58527 5.58958 6.9107L10.0003 11.3214L14.4111 6.91071C14.7365 6.58527 15.2641 6.58527 15.5896 6.91071C15.915 7.23614 15.915 7.76378 15.5896 8.08922L10.5896 13.0892C10.2641 13.4147 9.73651 13.4147 9.41107 13.0892L4.41107 8.08922C4.08563 7.76378 4.08563 7.23614 4.41107 6.9107Z"
+                            fill=""
+                          />
+                        </svg>
+                      </Link>
+
+                      {/* <!-- Dropdown Carriers Start --> */}
+                      <div
+                        className={`translate transform overflow-hidden ${
+                          !open && 'hidden'
+                        }`}
+                      >
+                        <ul className="mt-1 mb-1.5 flex flex-col gap-2.5 pl-6">
+                          <li>
+                            <Link
+                              href="/admin/carrier"
+                              className={`group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white ${
+                                pathname === '/admin/carrier' && 'text-white'
+                              }`}
+                            >
+                              Carrier
+                            </Link>
+                          </li>
+                        </ul>
+                      </div>
+                      {/* <!-- Dropdown Menu End --> */}
+
+                      {/* <!-- Dropdown Menu Start --> */}
+                      <div
+                        className={`translate transform overflow-hidden ${
+                          !open && 'hidden'
+                        }`}
+                      >
+                        <ul className="mt-1 mb-1.5 flex flex-col gap-2.5 pl-6">
+                          <li>
+                            <Link
+                              href="/admin/consignees"
+                              className={`group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white ${
+                                pathname === '/admin/consignee' && 'text-white'
+                              }`}
+                            >
+                              Consignees
+                            </Link>
+                          </li>
+                        </ul>
+                      </div>
+                      {/* <!-- Dropdown Menu End --> */}
+
+                      {/* <!-- Dropdown Users Start --> */}
+                      <div
+                        className={`translate transform overflow-hidden ${
+                          !open && 'hidden'
+                        }`}
+                      >
+                        <ul className="mt-1 mb-1.5 flex flex-col gap-2.5 pl-6">
+                          <li>
+                            <Link
+                              href="/admin/users"
+                              className={`group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white ${
+                                pathname === '/admin/users' && 'text-white'
+                              }`}
+                            >
+                              Users
+                            </Link>
+                          </li>
+                        </ul>
+                      </div>
+                      {/* <!-- Dropdown Users End --> */}
+
+                      {/* <!-- Dropdown Third Parties Start --> */}
+                      <div
+                        className={`translate transform overflow-hidden ${
+                          !open && 'hidden'
+                        }`}
+                      >
+                        <ul className="mt-1 mb-1.5 flex flex-col gap-2.5 pl-6">
+                          <li>
+                            <Link
+                              href="/admin/third-parties"
+                              className={`group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white ${
+                                pathname === '/admin/third-party' &&
+                                'text-white'
+                              }`}
+                            >
+                              Third Party
+                            </Link>
+                          </li>
+                        </ul>
+                      </div>
+                      {/* <!-- Dropdown Third Parties End --> */}
+                    </React.Fragment>
+                  );
+                }}
+              </SidebarLinkGroup>
+              {/* <!-- Admin Dropdown End --> */}
+
               <SidebarLinkGroup
                 activeCondition={
                   pathname === '/pages' || pathname.includes('pages')
