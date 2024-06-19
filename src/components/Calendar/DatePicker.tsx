82e09e6446135d1373161f061a6b17ced089fd94
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-06-19 12:02:11 -0700

    Update TextField style for DatePicker

--- src/components/Calendar/DatePicker.tsx
+++ src/components/Calendar/DatePicker.tsx
@@ -2,14 +2,31 @@ import React from 'react';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-
 import { styled } from '@mui/material/styles';
 import { TextField } from '@mui/material';
 
-/* You can style the `TextField` component directly */
+// does not use tailwind
+// does not handle darkMode
+
 const StyledTextField = styled(TextField)({
-  borderRadius: '8px',
-  border: '1px solid #01C37A',
+  '& .MuiOutlinedInput-root': {
+    color: '#919EAB', // there seems to be some transparency
+    fontSize: '16px',
+    lineHeight: '24px',
+    height: '56px',
+
+    '& fieldset': {
+      borderColor: '#DFE3E8',
+      borderRadius: '8px',
+    },
+    '&:hover fieldset': {
+      borderColor: '#DFE3E8',
+    },
+    '&.Mui-focused fieldset': {
+      outline: 'none',
+      border: '1px solid #DFE3E8',
+    },
+  },
 });
 
 export default function DatePicker() {
@@ -23,25 +40,3 @@ export default function DatePicker() {
     </LocalizationProvider>
   );
 }
-
-// // will have to pass props for selected day
-// // this does not use Tailwind
-
-// export default function DatePicker() {
-//   return (
-//     <LocalizationProvider dateAdapter={AdapterDayjs}>
-//       <DesktopDatePicker
-//         label="date picker"
-//         slotProps={{
-//           textField: {
-//             sx: {
-//               borderRadius: '8px',
-//               // borderColor: '#DFE3E8 !important',
-//               border: '1px solid #DFE3E8 !important',
-//             },
-//           },
-//         }}
-//       />
-//     </LocalizationProvider>
-//   );
-// }
