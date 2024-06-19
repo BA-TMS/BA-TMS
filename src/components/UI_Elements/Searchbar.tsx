25c66a54b025f0673757479ad754c519f9c31bad
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-06-19 13:45:57 -0700

    fix - searchbar height

--- src/components/UI_Elements/Searchbar.tsx
+++ src/components/UI_Elements/Searchbar.tsx
@@ -7,26 +7,25 @@ interface SearchbarProps {
 
 export default function Searchbar({ placeholder, onChange }: SearchbarProps) {
   return (
-    <form className="w-auto">
+    <div className="w-full max-w-96">
       <label
         htmlFor="default-search"
-        className="mb-2 body1 text-gray-900 sr-only dark:text-white"
+        className="body1 text-gray-900 sr-only dark:text-white"
       >
         Search
       </label>
-      <div className="relative">
+      <div className="h-14 relative">
         <div className="absolute inset-y-0 start-0 flex items-center ps-4 pointer-events-none">
           {SearchIcon}
         </div>
         <input
           type="search"
           id="default-search"
-          className="block w-full p-4 ps-10 body1 text-grey-900 placeholder-grey-500 border border-grey-300 rounded-lg  dark:bg-grey-800 dark:border-grey-700 dark:text-white focus:outline-none" // however, we should have focus visual indicators
+          className="block w-full h-full p-0 ps-12 pe-4 body1 text-grey-900 placeholder-grey-500 border border-grey-300 rounded-lg dark:bg-grey-800 dark:border-grey-700 dark:text-white box-border focus:outline-none"
           placeholder={placeholder}
           onChange={onChange}
-          //   required
         />
       </div>
-    </form>
+    </div>
   );
 }
