c6306fbb9c01c0e71f4d019417214c2a4f041872
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-06-26 12:16:01 -0700

    update - style for form date select component

--- src/components/UI_Elements/Form/DateSelect.tsx
+++ src/components/UI_Elements/Form/DateSelect.tsx
@@ -1,7 +1,7 @@
 import { UseControllerProps, useController } from 'react-hook-form';
 
 // component for date selector
-// needs some style updates
+// needs some style updates / integrate with the MUI date selector
 
 interface DateSelectProps extends UseControllerProps {
   control?: any;
@@ -12,19 +12,24 @@ const DateSelect = (props: DateSelectProps) => {
   const { field, fieldState } = useController(props);
 
   return (
-    <div className="mb-4.5">
-      <label className="mb-2.5 block text-black dark:text-white">
+    <div className="relative">
+      <input
+        type="date"
+        {...field}
+        className="block px-3 py-3.5 w-full body2 text-grey-800 dark:text-white bg-transparent rounded-[7px] border border-grey-400 appearance-none focus:outline-none focus:ring-0 focus:border-primary peer"
+      />
+
+      <label
+        htmlFor={props.name}
+        className="absolute body2 duration-300 transform -translate-y-4 top-2 z-10 origin-[0] text-grey-500 bg-white dark:text-white dark:bg-grey-900 px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-3"
+      >
         {props.name}
         {props.required && <span className="text-danger"> *</span>}
-        <input
-          type="date"
-          className="custom-input-date custom-input-date-1 w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary dark:white"
-          {...field}
-        />
-        {fieldState.error && (
-          <p className="mt-1 text-danger">{fieldState.error.message}</p>
-        )}
       </label>
+
+      {fieldState.error && (
+        <p className="mt-1 text-danger">{fieldState.error.message}</p>
+      )}
     </div>
   );
 };
