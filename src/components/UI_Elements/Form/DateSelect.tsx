fa9d202fd6bacf671932626ab1cd74fd9d50f924
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-06-27 12:05:02 -0700

    fix - form input error handling

--- src/components/UI_Elements/Form/DateSelect.tsx
+++ src/components/UI_Elements/Form/DateSelect.tsx
@@ -12,24 +12,28 @@ const DateSelect = (props: DateSelectProps) => {
   const { field, fieldState } = useController(props);
 
   return (
-    <div className="relative">
-      <input
-        type="date"
-        {...field}
-        className="block px-3 py-3.5 w-full body2 text-grey-800 dark:text-white bg-transparent rounded-[7px] border border-grey-400 appearance-none focus:outline-none focus:ring-0 focus:border-primary peer"
-      />
+    <div className="mb-1.5">
+      <div className="relative">
+        <input
+          type="date"
+          {...field}
+          className="block px-3 py-3.5 w-full body2 text-grey-800 dark:text-white bg-transparent rounded-[7px] border border-grey-400 appearance-none focus:outline-none focus:ring-0 focus:border-primary peer"
+        />
 
-      <label
-        htmlFor={props.name}
-        className="absolute body2 duration-300 transform -translate-y-4 top-2 z-10 origin-[0] text-grey-500 bg-white dark:text-white dark:bg-grey-900 px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-3"
-      >
-        {props.name}
-        {props.required && <span className="text-danger"> *</span>}
-      </label>
+        <label
+          htmlFor={props.name}
+          className="absolute body2 duration-300 transform -translate-y-4 top-2 z-10 origin-[0] text-grey-500 bg-white dark:text-white dark:bg-grey-900 px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-3"
+        >
+          {props.name}
+          {props.required && <span className="text-danger"> *</span>}
+        </label>
 
-      {fieldState.error && (
-        <p className="mt-1 text-danger">{fieldState.error.message}</p>
-      )}
+        <div className="min-h-5">
+          {fieldState.error && (
+            <p className="mt-1 text-danger">{fieldState.error.message}</p>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/UI_Elements/Form/TextInput.tsx
+++ src/components/UI_Elements/Form/TextInput.tsx
@@ -23,41 +23,44 @@ const TextInput = ({
   } = useController({ control, name });
 
   return (
-    <div className="relative">
-      {isTextArea ? (
-        <>
-          <textarea
-            {...field}
-            rows={rows || 3}
-            placeholder=" "
-            className="block px-3 py-3.5 w-full body2 text-grey-800 dark:text-white bg-transparent rounded-[7px] border border-grey-400 appearance-none focus:outline-none focus:ring-0 focus:border-primary peer"
-          ></textarea>
-          <label
-            htmlFor={name}
-            className="absolute body2 duration-300 transform -translate-y-0 scale-100 top-3 left-3 z-10 origin-[0] text-grey-500 bg-white dark:text-white dark:bg-grey-900 px-2 peer-focus:px-2 peer-placeholder-shown:top-3 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:top-2 peer-focus:-translate-y-4"
-          >
-            {name}
-            {required && <span className="text-error-dark"> *</span>}
-          </label>
-        </>
-      ) : (
-        <>
-          <input
-            {...field}
-            placeholder={field.name}
-            className="block px-3 py-3.5 w-full body2 text-grey-800 dark:text-white bg-transparent rounded-[7px] border border-grey-400 appearance-none focus:outline-none focus:ring-0 focus:border-primary peer"
-          />
-          <label
-            htmlFor={name}
-            className="absolute body2 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] text-grey-500 bg-white dark:text-white dark:bg-grey-900 px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-95 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-3"
-          >
-            {name}
-            {required && <span className="text-error-dark"> *</span>}
-          </label>
-        </>
-      )}
-
-      {error && <p className="caption mb-1 text-error-dark">{error.message}</p>}
+    <div className="mb-1.5">
+      <div className="relative">
+        {isTextArea ? (
+          <>
+            <textarea
+              {...field}
+              rows={rows || 3}
+              placeholder=""
+              className="block px-3 py-3.5 w-full body2 text-grey-800 dark:text-white bg-transparent rounded-[7px] border border-grey-400 appearance-none focus:outline-none focus:ring-0 focus:border-primary peer"
+            ></textarea>
+            <label
+              htmlFor={name}
+              className="absolute body2 duration-300 transform -translate-y-0 scale-100 top-3 left-3 z-10 origin-[0] text-grey-500 bg-white dark:text-white dark:bg-grey-900 px-2 peer-focus:px-2 peer-placeholder-shown:top-3 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:top-2 peer-focus:-translate-y-4"
+            >
+              {name}
+              {required && <span className="text-error-dark"> *</span>}
+            </label>
+          </>
+        ) : (
+          <>
+            <input
+              {...field}
+              placeholder={field.name}
+              className="block px-3 py-3.5 w-full body2 text-grey-800 dark:text-white bg-transparent rounded-[7px] border border-grey-400 appearance-none focus:outline-none focus:ring-0 focus:border-primary peer"
+            />
+            <label
+              htmlFor={name}
+              className="absolute body2 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] text-grey-500 bg-white dark:text-white dark:bg-grey-900 px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-95 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-3"
+            >
+              {name}
+              {required && <span className="text-error-dark"> *</span>}
+            </label>
+          </>
+        )}
+      </div>
+      <div className="min-h-5">
+        {error && <p className="caption text-error-dark">{error.message}</p>}
+      </div>
     </div>
   );
 };
