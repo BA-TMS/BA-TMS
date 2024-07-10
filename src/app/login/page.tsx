c7f5b93524e8b225a0c74842910c03cc78c3d2cd
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-07-10 14:05:58 -0700

    update - login page remove logo

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,7 +4,7 @@ import { login } from './actions';
 import { SubmitButton } from '@/components/Authentication/submit-button';
 import AuthButton from '@/components/Authentication/AuthButton';
 import Image from 'next/image';
-import Temp_Logo from '../../assets/Temp_Logo.png';
+import { Logo } from '@/assets/logo';
 
 export default async function Login({
   searchParams,
@@ -22,7 +22,7 @@ export default async function Login({
       <div className="flex justify-center items-center h-screen">
         <div className="border rounded-lg border-grey-300 p-10 w-full max-w-sm">
           <header className="flex flex-col justify-around items-center mb-3">
-            <Image src={Temp_Logo} alt="A2ZTMS Logo" priority />
+            {Logo}
             <p className="body2 dark:text-black my-6">Not {user.email}?</p>
             <AuthButton></AuthButton>
             <Link
@@ -41,11 +41,10 @@ export default async function Login({
     <div className="flex justify-center items-center h-screen">
       <div className="border rounded-lg border-grey-300 p-10 w-full max-w-sm">
         <header className="flex flex-col justify-between items-center mb-3">
-          <Image src={Temp_Logo} alt="A2ZTMS Logo" priority />
+          {Logo}
+
           <h1 className="text-title-md dark:text-black mt-6"> Welcome </h1>
-          <p className="body2 dark:text-black mt-4">
-            Log in to A2ZTMS to continue.
-          </p>
+          <p className="body2 dark:text-black mt-4">Log in to continue.</p>
         </header>
         <form className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
           <div className="relative">
