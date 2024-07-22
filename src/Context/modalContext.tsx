c861798e8cfad0437df60ed33cd06b856e931b5d
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-07-22 17:08:01 -0400

    modal context stores data if necessary

--- src/Context/modalContext.tsx
+++ src/Context/modalContext.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { createContext, useState } from 'react';
 
 // this is a context to avoid prop drilling through pages/ modals/ forms
@@ -9,19 +11,26 @@ interface ThemeProviderProps {
 // default values are initial state to provide
 export const ModalContext = createContext({
   isOpen: false,
-  toggleOpen: () => {},
+  data: null,
+  toggleOpen: (param?: any) => {},
 });
 
 export const ContextProvider: React.FC<ThemeProviderProps> = ({ children }) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [data, setData] = useState<any>(null);
 
   // how we open/ close said modal
-  function toggleOpen() {
+  // optional data if we are dealing with form updates
+  function toggleOpen(data = null) {
+    if (data !== null) {
+      setData(data);
+      console.log('toggle modal data', data);
+    }
     setIsOpen(!isOpen);
   }
 
   return (
-    <ModalContext.Provider value={{ isOpen, toggleOpen }}>
+    <ModalContext.Provider value={{ isOpen, toggleOpen, data }}>
       {children}
     </ModalContext.Provider>
   );
