44a8a915135f4fdf0b740b04c25ec565ba80d586
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-07-30 12:03:38 -0700

    delete load in slice

--- src/store/slices/loadSlice.ts
+++ src/store/slices/loadSlice.ts
@@ -1,24 +1,25 @@
 // slices/loadSlice.ts
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import { getLoads, addLoad as apiAddLoad } from '@/lib/dbActions';
+import {
+  getLoads,
+  addLoad as apiAddLoad,
+  deleteLoad as apiDeleteLoad,
+} from '@/lib/dbActions';
 
 // Define Async Thunks
-export const fetchLoads = createAsyncThunk(
-  'loads/fetchLoads',
-  async () => {
-    const data = await getLoads();
-    return data.map(load => ({
-      ...load,
-      shipDate: load.shipDate ? load.shipDate.toDateString() : null,
-      deliveryDate: load.deliveryDate ? load.deliveryDate.toDateString() : null,
-      carrier: load.carrier.name,
-      driver: load.driver ? load.driver.name : null,
-      customer: load.customer.name,
-      shipper: load.shipper ? load.shipper.name : null,
-      consignee: load.consignee ? load.consignee.name : null,
-    }));
-  }
-);
+export const fetchLoads = createAsyncThunk('loads/fetchLoads', async () => {
+  const data = await getLoads();
+  return data.map((load) => ({
+    ...load,
+    shipDate: load.shipDate ? load.shipDate.toDateString() : null,
+    deliveryDate: load.deliveryDate ? load.deliveryDate.toDateString() : null,
+    carrier: load.carrier.name,
+    driver: load.driver ? load.driver.name : null,
+    customer: load.customer.name,
+    shipper: load.shipper ? load.shipper.name : null,
+    consignee: load.consignee ? load.consignee.name : null,
+  }));
+});
 
 export const createLoad = createAsyncThunk(
   'loads/createLoad',
@@ -27,6 +28,13 @@ export const createLoad = createAsyncThunk(
     return newLoad;
   }
 );
+
+export const deleteLoad = createAsyncThunk('loads/deleteLoad', async (id) => {
+  // need to see what the load is
+  console.log('delete load id ', id);
+  const response = await apiDeleteLoad(id);
+  return response;
+});
 
 const loadSlice = createSlice({
   name: 'loads',
