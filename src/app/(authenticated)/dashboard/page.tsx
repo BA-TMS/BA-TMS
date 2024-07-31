e91631af8168131b12d1258e89fa19d55ec46947
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-07-31 11:21:09 -0700

    update - dashboard page coming soon

--- src/app/(authenticated)/dashboard/page.tsx
+++ src/app/(authenticated)/dashboard/page.tsx
@@ -1,14 +1,13 @@
 'use client';
 
 import { ContextProvider } from '@/Context/modalContext';
-import Load from '@/components/Table/Load';
 import Breadcrumbs from '@/components/Header/Breadcrumbs';
 
 const DashboardHome = () => {
   return (
     <ContextProvider>
       <div className="px-10 py-11.5 bg-grey-100 dark:bg-grey-800">
-        <h1 className="pagetitle">Dispatch Board</h1>
+        <h1 className="pagetitle">Dashboard</h1>
         <Breadcrumbs
           className="py-2 text-grey-600 dark:text-grey-300"
           root="Home"
@@ -16,8 +15,8 @@ const DashboardHome = () => {
           pathComponentClassName="hover:underline"
           capitalizePathComponents
         />
-        <Load />
       </div>
+      <h2 className="pl-10">Coming Soon</h2>
     </ContextProvider>
   );
 };
