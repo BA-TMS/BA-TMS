f89c95653905cfa469e68e175a4b16b1326c7f96
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-07-31 11:42:25 -0700

    update - dashboard/ dispatch pages with PageTitle component

--- src/app/(authenticated)/dashboard/page.tsx
+++ src/app/(authenticated)/dashboard/page.tsx
@@ -1,22 +1,13 @@
 'use client';
 
 import { ContextProvider } from '@/Context/modalContext';
-import Breadcrumbs from '@/components/Header/Breadcrumbs';
+import PageTitle from '@/components/Page/PageTitle';
 
 const DashboardHome = () => {
   return (
     <ContextProvider>
-      <div className="px-10 py-11.5 bg-grey-100 dark:bg-grey-800">
-        <h1 className="pagetitle">Dashboard</h1>
-        <Breadcrumbs
-          className="py-2 text-grey-600 dark:text-grey-300"
-          root="Home"
-          separator={<p className="mx-2">&#8226;</p>}
-          pathComponentClassName="hover:underline"
-          capitalizePathComponents
-        />
-      </div>
-      <h2 className="pl-10">Coming Soon</h2>
+      <PageTitle pageTitle="Dashboard" />
+      <h3 className="pl-10 py-10">Coming Soon</h3>
     </ContextProvider>
   );
 };
--- src/app/(authenticated)/dispatch/page.tsx
+++ src/app/(authenticated)/dispatch/page.tsx
@@ -2,22 +2,14 @@
 
 import { ContextProvider } from '@/Context/modalContext';
 import Load from '@/components/Table/Load';
-import Breadcrumbs from '@/components/Header/Breadcrumbs';
+
+import PageTitle from '@/components/Page/PageTitle';
 
 const DispatchPage = () => {
   return (
     <ContextProvider>
-      <div className="px-10 py-11.5 bg-grey-100 dark:bg-grey-800">
-        <h1 className="pagetitle">Dispatch Board</h1>
-        <Breadcrumbs
-          className="py-2 text-grey-600 dark:text-grey-300"
-          root="Home"
-          separator={<p className="mx-2">&#8226;</p>}
-          pathComponentClassName="hover:underline"
-          capitalizePathComponents
-        />
-        <Load />
-      </div>
+      <PageTitle pageTitle="Dispatch Board" />
+      <Load />
     </ContextProvider>
   );
 };
