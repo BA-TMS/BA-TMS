6483ea67fff20d16436ba680e8252aaba7a4d584
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-07-31 11:17:07 -0700

    move dispatch page to /dispatch + update sidebar link

---
+++ src/app/(authenticated)/dispatch/page.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { ContextProvider } from '@/Context/modalContext';
+import Load from '@/components/Table/Load';
+import Breadcrumbs from '@/components/Header/Breadcrumbs';
+
+const DispatchPage = () => {
+  return (
+    <ContextProvider>
+      <div className="px-10 py-11.5 bg-grey-100 dark:bg-grey-800">
+        <h1 className="pagetitle">Dispatch Board</h1>
+        <Breadcrumbs
+          className="py-2 text-grey-600 dark:text-grey-300"
+          root="Home"
+          separator={<p className="mx-2">&#8226;</p>}
+          pathComponentClassName="hover:underline"
+          capitalizePathComponents
+        />
+        <Load />
+      </div>
+    </ContextProvider>
+  );
+};
+
+export default DispatchPage;
