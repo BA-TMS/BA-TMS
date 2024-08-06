705feef54a9d3b134b03f1837099f88115f33599
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-08-06 11:57:02 -0700

    create customers page

---
+++ src/app/(authenticated)/customers/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import TabGroupOne from '@/components/Dashboard/TabGroupOne';
+
+const GroupOne: React.FC = () => {
+  return <TabGroupOne />;
+};
+
+export default GroupOne;
