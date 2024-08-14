a35c3bbb1228997cc79b79c0ddb643d041bf4f66
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-08-14 11:48:17 -0700

    tab style

--- src/components/UI_Elements/Form/Tabs.tsx
+++ src/components/UI_Elements/Form/Tabs.tsx
@@ -11,25 +11,28 @@ interface TabProps {
 }
 
 export function TabsComponent({ children }) {
-  const initialTab = children[0].label;
-  const [activeTab, setActiveTab] = useState(initialTab); // hold active tab's label
+  const initialTab = children[0].props.label;
+  const [activeTab, setActiveTab] = useState(initialTab);
+
+  // memoized handler for changing active tab
   const handleActiveTab = useCallback(
     (label: unknown) => setActiveTab(label),
     []
-  ); // memoized handler for changing active tab
+  );
 
   // dynamically define tabs and content
-  const tabs = children.map((child) => (
+  const tabs = children.map((child: JSX.Element) => (
     <button
+      className="subtitle2 text-grey-600 dark:text-grey-200 hover:text-grey-800 dark:hover:text-primary cursor-pointer py-2 border-b-2 border-transparent hover:border-b-2 hover:border-grey-800 dark:hover:border-primary"
       onClick={(e) => {
         e.preventDefault();
         handleActiveTab(child.props.label);
       }}
-      className={
-        child.props.label === activeTab
-          ? ['tabs__tab', 'tabs__tab-active'].join(' ')
-          : 'tabs__tab'
-      }
+      //   className={
+      //     child.props.label === activeTab
+      //       ? ['tabs__tab', 'tabs__tab-active'].join(' ')
+      //       : 'tabs__tab'
+      //   }
       key={child.props.label}
     >
       {child.props.tabName}
@@ -38,14 +41,16 @@ export function TabsComponent({ children }) {
 
   // iterate children again and only hold child with same label as active tab
   const tabContent = children.filter(
-    (child) => child.props.label === activeTab
+    (child: JSX.Element) => child.props.label === activeTab
   );
 
   return (
-    <div>
-      <div className="tabs__box">{tabs}</div>
+    <>
+      <div className="px-6 h-12 bg-grey-200 dark:bg-grey-700 flex items-center justify-start content-between gap-10 overflow-hidden">
+        {tabs}
+      </div>
       <div>{tabContent}</div>
-    </div>
+    </>
   );
 }
 
