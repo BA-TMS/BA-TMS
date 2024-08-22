e55f7d321d0a9624dbac16d1e43e86fe5445a717
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-08-22 11:29:16 -0700

    fix types + add formValues to context

--- src/Context/modalContext.tsx
+++ src/Context/modalContext.tsx
@@ -1,23 +1,48 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { createContext, useState } from 'react';
 
 // this is a context to avoid prop drilling through pages/ modals/ forms
+// save form data state and access it here
+
+interface ModalContextType {
+  isOpen: boolean;
+  data: any; // Replace with appropriate type
+  toggleOpen: (param?: any) => void;
+  formData: FormObject<any>;
+  saveFormValues: (values: FormObject<any>) => void;
+}
 
 interface ThemeProviderProps {
   children: React.ReactNode;
 }
 
-// default values are initial state to provide
-export const ModalContext = createContext({
+type FormObject<T = any> = {
+  [key: string]: T;
+};
+
+export const ModalContext = createContext<ModalContextType>({
   isOpen: false,
   data: null,
-  toggleOpen: (param?: any) => {},
+  toggleOpen: () => {},
+  formData: {},
+  saveFormValues: () => {},
 });
 
 export const ContextProvider: React.FC<ThemeProviderProps> = ({ children }) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  // get rid of this one eventually
   const [data, setData] = useState<any>(null);
+
+  // NEW FORM DATA
+  const [formData, setFormData] = useState<FormObject<any>>({});
+
+  // EDITING NEW FORM DATA
+  const saveFormValues = (values: FormObject<any>) => {
+    console.log('saving values in context', values);
+    setFormData({ ...formData, ...values });
+    console.log('the new form', formData);
+    // have to clear eventually
+  };
 
   // how we open/ close said modal
   // optional data if we are dealing with form updates
@@ -29,7 +54,9 @@ export const ContextProvider: React.FC<ThemeProviderProps> = ({ children }) => {
   }
 
   return (
-    <ModalContext.Provider value={{ isOpen, toggleOpen, data }}>
+    <ModalContext.Provider
+      value={{ isOpen, toggleOpen, data, formData, saveFormValues }}
+    >
       {children}
     </ModalContext.Provider>
   );
