6dc0ec5be12e3a6f6eae6468e9afc1af43791aca
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-09-25 12:29:10 -0700

    fixing the route/ modal issue

--- src/app/(authenticated)/customers/@modal/(.)add-customer/page.tsx
+++ src/app/(authenticated)/customers/@modal/(.)add-customer/page.tsx
@@ -4,5 +4,5 @@ import { redirect } from 'next/navigation';
 // we shouldn't see this page
 
 export default function AddCustomerModal() {
-  return redirect('/customers/add-customer/details');
+  return redirect('/customers');
 }
--- src/app/(authenticated)/customers/add-customer/page.tsx
+++ src/app/(authenticated)/customers/add-customer/page.tsx
@@ -3,5 +3,5 @@ import { redirect } from 'next/navigation';
 // we shouldn't see this page
 
 export default function AddCustomerModal() {
-  return redirect('/customers/add-customer/details');
+  return redirect('/customers');
 }
