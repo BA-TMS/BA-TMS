579c5e945cf38ebcbdc245deba88b8ec539cead8
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-10-09 14:10:58 -0700

    update display appearance

--- src/components/Forms/Customer/ViewCustomer.tsx
+++ src/components/Forms/Customer/ViewCustomer.tsx
@@ -2,8 +2,9 @@
 
 import { useContext } from 'react';
 import { ModalContext } from '@/Context/modalContext';
-import { CustomerData } from '@/types/customerTypes';
+import { CustomerData, customerFieldMap } from '@/types/customerTypes';
 import Button from '@/components/UI_Elements/buttons/Button';
+import DataDisplay from '@/components/UI_Elements/Display/DataDisplay';
 import { useRouter } from 'next/navigation';
 
 // component will need to take in a customer to map data in a way to display it
@@ -20,36 +21,23 @@ const ViewCustomer = ({ data }: ViewCustomerProps) => {
   const { saveFormValues } = useContext(ModalContext);
 
   return (
-    <div className="flex flex-col justify-between">
-      <div className="">
-        <div className="flex flex-col gap-5 xl:flex-row">
-          <div className="flex flex-col w-full xl:w-1/2">
-            <p> one column </p>
-          </div>
-          <div className="flex flex-col w-full xl:w-1/2">
-            <p>another column</p>
-          </div>
-        </div>
+    <div className="flex flex-col">
+      <div className="flex flex-col self-center w-3/4">
+        {/* map */}
 
-        <p> more </p>
+        {Object.keys(customerFieldMap).map((value) => {
+          const customerFieldKey = customerFieldMap[value];
 
-        <div className="flex flex-col gap-5 xl:flex-row">
-          <div className="flex flex-col w-full xl:w-1/2">
-            <p>ahh</p>
-          </div>
-          <div className="flex flex-col w-full xl:w-1/2">
-            <p>ahh</p>
-          </div>
-        </div>
+          return (
+            <DataDisplay
+              title={value}
+              text={data[customerFieldKey] || ''}
+              key={customerFieldKey}
+            />
+          );
+        })}
       </div>
 
-      {/* <div className="min-h-5">
-          {errors.root && (
-            <p className="caption mb-1 text-error-dark">
-              {errors.root.message}
-            </p>
-          )}
-        </div> */}
       <div className="py-3.5 gap-2 border-t border-grey-300 dark:border-grey-700 flex justify-between sticky bottom-0 bg-white dark:bg-grey-900 z-10">
         <Button
           type="button"
--- src/components/UI_Elements/Display/DataDisplay.tsx
+++ src/components/UI_Elements/Display/DataDisplay.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 'use client';
 
 // this component displays information
@@ -5,16 +6,16 @@
 // optional props
 interface DisplayProps {
   title: string;
-  text: string;
+  text: any;
 }
 
 const DataDisplay = ({ title, text }: DisplayProps) => {
   return (
-    <div className="mt-5 mb-1.5">
+    <div className="mt-2.5 mb-1 px-1.5 pb-1.5 flex justify-between items-center w-full border-b border-grey-300 dark:border-grey-700">
       <p className="subtitle1 text-grey-800 dark:text-white bg-transparent">
         {title}
       </p>
-      <p className="block px-3 py-2 w-full body1 text-grey-800 dark:text-white bg-transparent">
+      <p className="py-1 body1 text-grey-800 dark:text-white bg-transparent">
         {text}
       </p>
     </div>
