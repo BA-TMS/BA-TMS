0a2f40cdae6e3a217d09fd5cfcd1df6145f586b4
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-10-10 10:34:42 -0700

    view customer modal page

--- src/app/(authenticated)/customers/@modal/(.)view/[id]/page.tsx
+++ src/app/(authenticated)/customers/@modal/(.)view/[id]/page.tsx
@@ -1,55 +1,36 @@
 'use client';
 
+import { useSelector } from 'react-redux';
+import { usePathname } from 'next/navigation';
 import Modal from '@ui/Modal/Modal';
 import ViewCustomer from '@/components/Forms/Customer/ViewCustomer';
+import { RootState } from '@/store/store';
+import { CustomerData } from '@/types/customerTypes';
 
 // this is an intercepting route that builds a modal
 // it uses dynamic routing as we don't know what the id is
+// takes in the id to find the entry
 
 export default function ViewCustomerModal() {
-  // get the customer information so we can see it
-  // will have to fetch from the database/ pass info?
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const customer = {
-    id: 'c8f660e0-4bdd-47b7-a400-d94fd7e13e87',
-    createdAt: '2024-10-02T20:55:36.078Z',
-    updatedAt: '2024-10-02T20:55:36.078Z',
-    status: 'ACTIVE',
-    companyName: 'Company',
-    contactName: 'Lane Hamilton',
-    secondaryContactName: '',
-    salesRepName: '',
-    contactEmail: 'test@example.us',
-    contactTelephone: '6142845263',
-    contactTollFree: '',
-    contactFax: '',
-    contactCountry: 'United States',
-    contactAddress: '1600 Amphitheatre Parkway',
-    contactAddressField2: '1600 Amphitheatre Parkway',
-    contactAddressField3: '1600 Amphitheatre Parkway',
-    contactCity: 'Mountain View',
-    contactState: 'Alaska',
-    contactPostCode: '94043',
-    billingEmail: 'test@example.us',
-    billingTelephone: '6142845263',
-    billingCountry: 'United States',
-    billingAddress: '1600 Amphitheatre Parkway',
-    billingAddressField2: '1600 Amphitheatre Parkway',
-    billingAddressField3: '1600 Amphitheatre Parkway',
-    billingCity: 'Mountain View',
-    billingState: 'Alaska',
-    billingPostCode: '94043',
-    currency: '',
-    paymentTerms: '1.5/2 Net 30',
-    creditLimit: '5000',
-    federalID: '66-55555',
-    factorId: null,
-    factor: null,
-  };
+  const pathname = usePathname();
+
+  // slice the pathname to get the id
+  const customerId = pathname.split('/view/')[1];
+
+  // use the id to pull from redux
+  const customer = useSelector((state: RootState) =>
+    state.customers.items.find(
+      (customer: CustomerData) => customer.id === customerId
+    )
+  );
 
   return (
     <Modal title={'View Customer'}>
-      <ViewCustomer data={customer} />
+      {customer ? (
+        <ViewCustomer data={customer} />
+      ) : (
+        <p className="caption my-1 text-error-dark">Something went wrong.</p> // change to skeleton
+      )}
     </Modal>
   );
 }
