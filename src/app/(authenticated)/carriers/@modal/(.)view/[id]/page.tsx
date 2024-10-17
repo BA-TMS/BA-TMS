a5bf0a710bdc39af37b21426142791b8c3e4f95d
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-10-17 13:35:11 -0700

    setup view carrier modal

---
+++ src/app/(authenticated)/carriers/@modal/(.)view/[id]/page.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { useSelector } from 'react-redux';
+import { usePathname } from 'next/navigation';
+import Modal from '@ui/Modal/Modal';
+import ViewCustomer from '@/components/Forms/Customer/ViewCustomer';
+import ViewCustomerSkeleton from '@/components/Forms/Customer/ViewCustomerSkeleton';
+import { RootState } from '@/store/store';
+import { CustomerData } from '@/types/customerTypes';
+
+// this is an intercepting route that builds a modal
+// it uses dynamic routing as we don't know what the id is
+// takes in the id to find the entry
+
+export default function ViewCustomerModal() {
+  const pathname = usePathname();
+
+  // slice the pathname to get the id
+  const carrierId = pathname.split('/view/')[1];
+
+  // use the id to pull from redux
+  //   const customer = useSelector((state: RootState) =>
+  //     state.customers.items.find(
+  //       (customer: CustomerData) => customer.id === customerId
+  //     )
+  //   );
+
+  return (
+    <Modal title={'View External Carrier'}>
+      {/* {customer ? <ViewCustomer data={customer} /> : <ViewCustomerSkeleton />} */}
+      <h2>Placeholder for view carrier component</h2>
+    </Modal>
+  );
+}
