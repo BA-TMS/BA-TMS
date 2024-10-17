ded399609428e3ab7dfd6cdc2568f218924bedbf
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-10-17 12:39:51 -0700

    @modal default + page

---
+++ src/types/carrierTypes.ts
@@ -0,0 +1,14 @@
+export type Carrier = {
+  name: string;
+  address: string;
+  addressAddOn: string | null;
+  city: string;
+  state: string;
+  postCountry: string;
+  postCode: string;
+  telCountry: string;
+  telephone: string;
+  dotId: string;
+  factorId: string | null;
+  taxId: string;
+};
