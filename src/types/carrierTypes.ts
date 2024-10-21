2ef676883276aaf94d7e982aa6bca4c273b95dd7
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-10-21 13:13:34 -0700

    carrier data types

--- src/types/carrierTypes.ts
+++ src/types/carrierTypes.ts
@@ -1,14 +1,69 @@
-export type CarrierData = {
-  name: string;
+export interface CarrierData {
+  id: string;
+  createdAt: Date | string;
+  updatedAt: Date | string;
+  status: string; // "ACTIVE" | "INACTIVE" ??
+
+  carrierName: string;
   address: string;
-  addressAddOn: string | null;
+  addressField2: string | null;
+  addressField3: string | null;
   city: string;
   state: string;
   postCountry: string;
   postCode: string;
-  telCountry: string;
-  telephone: string;
+
+  contactName: string | null;
+  contactEmail: string | null;
+  contactTelephone: string;
+  contactTollFree: string | null;
+  contactFax: string | null;
+
+  paymentTerms: string;
+  taxId: string | null;
+  docketNumType: string;
+  docketNumber: string;
+  ursNumber: string | null;
   dotId: string;
+
+  factor: { name: string } | null; // i think
   factorId: string | null;
-  taxId: string;
-};
+
+  notes: string;
+}
+
+export interface CarrierInsuranceData {
+  id: string;
+  // carrier   Carrier  @relation(fields: [carrierId], references: [id])
+  carrier: { name: string };
+  // carrierId String   @unique
+  carrierId: string | null;
+  createdAt: Date | string;
+  updatedAt: Date | string;
+
+  liabilityCompany: string | null;
+  liabilityPolicy: string | null;
+  liabilityExpiration: Date | null;
+  liabilityTelephone: string | null;
+  liabilityContact: string | null;
+
+  autoInsCompany: string | null;
+  autoInsPolicy: string | null;
+  autoInsExpiration: Date | null;
+  autoInsTelephone: string | null;
+  autoInsContact: string | null;
+
+  cargoCompany: string | null;
+  cargoPolicy: string | null;
+  cargoExpiration: Date | null;
+  cargoTelephone: string | null;
+  cargoContact: string | null;
+  cargoWSIB: string | null;
+
+  fmcsaInsCompany: string;
+  fmcsaInsPolicy: string;
+  fmcsaInsExpiration: Date | null;
+  fmcsaType: string;
+  fmcsaCoverage: string;
+  fmcsaTelephone: string;
+}
