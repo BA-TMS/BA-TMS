5c36823703cb891248a8acaf95d00029f5507fe3
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-10-22 13:48:58 -0700

    carrier form setup

--- src/components/Forms/Carrier/CarrierForm.tsx
+++ src/components/Forms/Carrier/CarrierForm.tsx
@@ -1,171 +1,328 @@
 'use client';
 
-import { useEffect, useContext } from 'react';
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import * as yup from 'yup';
-import { yupResolver } from '@hookform/resolvers/yup';
-import TextInput from '@/components/UI_Elements/Form/TextInput';
-import SelectInput from '@mui/material/Select/SelectInput';
-import DynamicSelect from '@/components/UI_Elements/Form/DynamicSelect';
-import { usStates } from '@/components/Forms/data/states';
-import { getFactor } from '@/lib/dbActions';
+import React, { useEffect, useState, useContext } from 'react';
+import Button from '../../UI_Elements/buttons/Button';
 import { ModalContext } from '@/Context/modalContext';
 import { createCarrier } from '@/store/slices/carrierSlice';
 import { useDispatch } from 'react-redux';
+import { AppDispatch } from '@/store/store';
+import DataDisplay from '@/components/UI_Elements/Display/DataDisplay';
+import AddressDisplay from '@/components/UI_Elements/Display/AddressDisplay';
+import { useRouter } from 'next/navigation';
+import { CarrierData } from '@/types/carrierTypes';
 
 // this component submits form data from the context to database using redux
 
-const carrierSchema = yup.object({
-  'Carrier Name': yup.string().required('Carrier Name is required'),
-  Address: yup.string().required('Address is required'),
-  'Address Line 2': yup.string(),
-  City: yup.string().required('City is required '),
-  State: yup.string().required('State is required '),
-  Zip: yup
-    .string()
-    .matches(/^\d{5}$/, 'Zip must be 5 digits')
-    .required('Zip Code is required '),
-  Country: yup.string().required('Country is required'),
-  'Country Code': yup
-    .string()
-    .matches(/^\d+$/, 'Must be a valid Country Code')
-    .required('Country Code is required'),
-  'Phone Number': yup
-    .string()
-    .matches(/^\d{10}$/, 'Must use valid phone number')
-    .required('Contact phone number required'),
-  'DOT ID': yup
-    .string()
-    .matches(/^\d+$/, 'Must be a valid DOT ID')
-    .required('Must enter DOT ID')
-    .min(6, 'Must be valid DOT ID')
-    .max(8, 'Must be valid DOT ID'),
-  'Factor ID': yup.string().required('Must include Factor ID'),
-  'Tax ID': yup.string(),
-  Notes: yup.string().max(250, 'Must be under 250 characters'),
-});
+// simplify submission process- do we need to validate?
 
-type Carrier = yup.InferType<typeof carrierSchema>;
+// on submit- add carrier using redux, submit insurance data to db
+// i don't think insurance data needs to be stored in redux
 
 export const CarrierForm = () => {
-  const dispatch = useDispatch();
-  const {
-    handleSubmit,
-    setError,
-    reset,
-    control,
-    formState: { errors, isSubmitting, isSubmitSuccessful },
-  } = useForm<Carrier>({
-    defaultValues: {
-      'Carrier Name': '',
-      Address: '',
-      'Address Line 2': '',
-      City: '',
-      State: '',
-      Zip: '',
-      Country: '',
-      'Country Code': '',
-      'Phone Number': '',
-      'DOT ID': '',
-      'Factor ID': '',
-      'Tax ID': '',
-      Notes: '',
-    },
-    resolver: yupResolver(carrierSchema),
-  });
+  const dispatch = useDispatch<AppDispatch>();
 
-  const { toggleOpen } = useContext(ModalContext);
+  const router = useRouter();
 
-  const onSubmit = async (data: Carrier) => {
-    try {
-      await dispatch(createCarrier(data)).unwrap();
-      reset();
-      toggleOpen();
-    } catch (error) {
-      console.error('Error submitting carrier create form:', error);
-    }
+  const { formData, saveFormValues } = useContext(ModalContext);
+  console.log('CARRIER FORM DATA', formData);
+
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const [isUpdate, setIsUpdate] = useState<boolean>(false);
+
+  const onSubmit = async (carrier: CarrierData) => {
+    setIsSubmitting(true);
+    setError(''); // clear previous errors
   };
 
+  // in the event of an update
+  // we're goung to have to check what kind of id this is (carrier or carrier insurance)
   useEffect(() => {
-    if (isSubmitSuccessful) {
-      reset({});
+    if (formData !== null && formData['id']) {
+      setIsUpdate(true);
     }
-  }, [isSubmitSuccessful, reset]);
+  }, [formData]);
 
   return (
-    <div className="flex flex-col gap-9">
-      <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark w-full max-w-xl mx-auto overflow-y-auto max-h-screen">
-        <div className="border-b border-stroke py-4 px-6.5 dark:border-strokedark">
-          <h3 className="font-medium text-black dark:text-white">
-            New Carrier
-          </h3>
-        </div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="p-6.5">
-            <TextInput control={control} name="Carrier Name" required={true} />
-            <TextInput control={control} name="Address" required={true} />
-            <TextInput control={control} name="Address Line 2" />
+    <div className="flex flex-col h-full">
+      <div className="flex-grow">
+        <p className="my-3.5 body2 text-grey-800 dark:text-white">
+          Confirm Carrier Details
+        </p>
 
-            <div className=" flex flex-col gap-6 xl:flex-row">
-              <div className="w-full xl:w-1/2">
-                <TextInput control={control} name="City" required={true} />
-                <SelectInput
-                  control={control}
-                  name="State"
-                  options={usStates}
-                  required={true}
-                />
-                <TextInput
-                  control={control}
-                  name="Country Code"
-                  required={true}
-                />
-                <TextInput control={control} name="DOT ID" required={true} />
-              </div>
-              <div className="w-full xl:w-1/2">
-                <TextInput control={control} name="Zip" required={true} />
-                <TextInput control={control} name="Country" required={true} />
-                <TextInput
-                  control={control}
-                  name="Phone Number"
-                  required={true}
-                />
-                <DynamicSelect
-                  control={control}
-                  name="Factor ID"
-                  dbaction={getFactor}
-                  required={true}
-                ></DynamicSelect>
-              </div>
-            </div>
-            <TextInput control={control} name="Tax ID" />
-            <TextInput control={control} name="Notes" isTextArea={true} />
-            {errors.root && (
-              <p className="mb-5 text-danger">{errors.root.message}</p>
-            )}
-            <div className="flex justify-end">
-              <button
-                type="submit"
-                disabled={isSubmitting}
-                className="w-1/4 rounded bg-primary p-3 font-medium text-gray hover:bg-opacity-80"
-              >
-                {isSubmitting ? 'Submitting' : 'Add'}
-              </button>
-              <button
-                type="button"
-                onClick={() => {
-                  reset();
-                  toggleOpen();
-                }}
-                disabled={isSubmitting}
-                className="rounded bg-red p-3 font-medium text-gray ml-2 hover:bg-opacity-80"
-              >
-                Cancel
-              </button>
-            </div>
+        <div className="mt-5 flex flex-col gap-5 xl:flex-row">
+          <div className=" w-full">
+            <DataDisplay title="Carrier Name" text={formData['Carrier Name']} />
           </div>
-        </form>
+          <div className="w-full md:w-1/3">
+            <DataDisplay title="Status" text={formData['Status']} />
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-5 xl:flex-row">
+          <div className="flex flex-col w-full xl:w-1/2">
+            <DataDisplay title="Contact Name" text={formData['Contact Name']} />
+          </div>
+
+          <div className="flex flex-col w-full xl:w-1/2">
+            <DataDisplay
+              title="Contact Email"
+              text={formData['Contact Email']}
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-4 md:flex-row">
+          <div className="w-full md:w-1/3">
+            <DataDisplay title="Telephone" text={formData['Telephone']} />
+          </div>
+          <div className="w-full md:w-1/3">
+            <DataDisplay title="Toll Free" text={formData['Toll Free']} />
+          </div>
+          <div className="w-full md:w-1/3">
+            <DataDisplay title="Fax" text={formData['Fax']} />
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-5 xl:flex-row">
+          <div className="flex flex-col self-center w-full">
+            <DataDisplay
+              title="Payment Terms"
+              text={formData['Payment Terms']}
+            />
+            <DataDisplay
+              title="Docket Number"
+              text={
+                formData['Docket Number Type']
+                  ? formData['Docket Number Type'] +
+                    ' ' +
+                    formData['Docket Number']
+                  : ''
+              }
+            />
+            <DataDisplay title="DOT ID#" text={formData['DOT ID#']} />
+          </div>
+
+          <div className="flex flex-col self-center w-full">
+            <DataDisplay title="URS #" text={formData['URS #']} />
+            <DataDisplay title="Tax ID#" text={formData['Tax ID#']} />
+
+            <DataDisplay title="Factoring Company" text={''} />
+          </div>
+        </div>
+
+        <div className="flex flex-col self-center w-full">
+          <AddressDisplay
+            title={'Address'}
+            addressLine1={formData['Address']}
+            addressLine2={formData['Address Line 2']}
+            addressLine3={formData['Address Line 3']}
+            city={formData['City']}
+            state={formData['State']}
+            zip={formData['Zip']}
+            country={formData['Country']}
+          />
+        </div>
+
+        <div className="flex flex-col gap-5 xl:flex-row">
+          <div className="flex flex-col w-full xl:w-1/2">
+            <DataDisplay
+              title="FMCSA Insurance Company"
+              text={formData['FMCSA Insurance Company']}
+            />
+          </div>
+
+          <div className="flex flex-col w-full xl:w-1/2">
+            <DataDisplay
+              title="FMCSA Policy #"
+              text={formData['FMCSA Policy #']}
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-4 md:flex-row">
+          <div className="w-full md:w-1/3">
+            <DataDisplay title="FMCSA Type" text={formData['FMCSA Type']} />
+          </div>
+          <div className="w-full md:w-1/3">
+            <DataDisplay
+              title="FMCSA Coverage $"
+              text={formData['FMCSA Coverage $']}
+            />
+          </div>
+          <div className="w-full md:w-1/3">
+            <DataDisplay
+              title="FMCSA Expiration Date"
+              text={formData['FMCSA Expiration Date']}
+            />
+          </div>
+        </div>
+
+        {/* Liability Insurance */}
+        <div className="mt-5 flex flex-col gap-5 xl:flex-row">
+          <div className="flex flex-col w-full xl:w-1/2">
+            <DataDisplay
+              title="Liability Insurance Company"
+              text={formData['Liability Insurance Company']}
+            />
+          </div>
+
+          <div className="flex flex-col w-full xl:w-1/2">
+            <DataDisplay
+              title="Liability Policy #"
+              text={formData['Liability Policy #']}
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-4 md:flex-row">
+          <div className="w-full md:w-1/3">
+            <DataDisplay
+              title="Liability Contact"
+              text={formData['Liability Contact']}
+            />
+          </div>
+          <div className="w-full md:w-1/3">
+            <DataDisplay
+              title="Liability Telephone"
+              text={formData['Liability Telephone']}
+            />
+          </div>
+          <div className="w-full md:w-1/3">
+            <DataDisplay
+              title="Liability Expiration Date"
+              text={formData['Liability Expiration Date']}
+            />
+          </div>
+        </div>
+
+        {/* Auto Insurance */}
+        <div className="mt-5 flex flex-col gap-5 xl:flex-row">
+          <div className="flex flex-col w-full xl:w-1/2">
+            <DataDisplay
+              title="Auto Insurance Company"
+              text={formData['Auto Insurance Company']}
+            />
+          </div>
+
+          <div className="flex flex-col w-full xl:w-1/2">
+            <DataDisplay
+              title="Auto Policy #"
+              text={formData['Auto Policy #']}
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-4 md:flex-row">
+          <div className="w-full md:w-1/3">
+            <DataDisplay title="Auto Contact" text={formData['Auto Contact']} />
+          </div>
+          <div className="w-full md:w-1/3">
+            <DataDisplay
+              title="Auto Telephone"
+              text={formData['Auto Telephone']}
+            />
+          </div>
+          <div className="w-full md:w-1/3">
+            <DataDisplay
+              title="Auto Expiration Date"
+              text={formData['Auto Expiration Date']}
+            />
+          </div>
+        </div>
+
+        {/* Cargo Company */}
+        <div className="mt-5 flex flex-col gap-5 xl:flex-row">
+          <div className="flex flex-col w-full xl:w-1/2">
+            <DataDisplay
+              title="Cargo Company"
+              text={formData['Cargo Company']}
+            />
+          </div>
+
+          <div className="flex flex-col w-full xl:w-1/2">
+            <DataDisplay
+              title="Cargo Policy #"
+              text={formData['Cargo Policy #']}
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-4 md:flex-row">
+          <div className="w-full md:w-1/3">
+            <DataDisplay
+              title="Cargo Contact"
+              text={formData['Cargo Contact']}
+            />
+          </div>
+          <div className="w-full md:w-1/3">
+            <DataDisplay
+              title="Cargo Telephone"
+              text={formData['Cargo Telephone']}
+            />
+          </div>
+          <div className="w-full md:w-1/3">
+            <DataDisplay title="Cargo WSIB #" text={formData['Cargo WSIB #']} />
+          </div>
+          <div className="w-full md:w-1/3">
+            <DataDisplay
+              title="Cargo Expiration"
+              text={formData['Cargo Expiration Date']}
+            />
+          </div>
+        </div>
+
+        <DataDisplay title="Notes" text={formData['Notes']} />
+
+        <div className="py-3.5 gap-2 border-t border-grey-300 dark:border-grey-700 flex justify-between sticky bottom-0 bg-white dark:bg-grey-900 z-10">
+          {error && (
+            <div className="min-h-5 mr-2 self-center">
+              <p className="caption mb-1 text-error-dark">{error}</p>
+            </div>
+          )}
+          <Button
+            id="cancel"
+            type="button"
+            disabled={isSubmitting}
+            onClick={() => {
+              const cancel = confirm('Cancel this entry?');
+              if (cancel) {
+                saveFormValues({}, true); // clears context values
+                router.push('/customers');
+              } else return;
+            }}
+            variant="outline"
+            intent="default"
+          >
+            Cancel
+          </Button>
+
+          <div className="flex justify-end gap-2">
+            <Button
+              type="button"
+              variant="outline"
+              intent="success"
+              disabled={isSubmitting}
+              onClick={() => {
+                router.back();
+              }}
+            >
+              Back
+            </Button>
+            <Button
+              type="submit"
+              disabled={isSubmitting}
+              onClick={() => {
+                console.log('SUBMITTING');
+                // onSubmit(formData as CarrierData);
+                saveFormValues({}, true); // Reset form data after submission
+                router.push('/carriers');
+              }}
+            >
+              {isSubmitting ? 'Submitting...' : isUpdate ? 'Update' : 'Add'}
+            </Button>
+          </div>
+        </div>
       </div>
     </div>
   );
