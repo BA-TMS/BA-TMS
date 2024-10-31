7622a27f2b71bd4c953f84c075dbbf70e1459d1f
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-10-31 13:46:52 -0700

    company form

--- src/app/signup/company/page.tsx
+++ src/app/signup/company/page.tsx
@@ -1,200 +1,128 @@
 'use client';
+
+import { useContext } from 'react';
 import * as yup from 'yup';
-import YupPassword from 'yup-password';
-YupPassword(yup);
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useForm } from 'react-hook-form';
+import { ModalContext } from '@/Context/modalContext';
+import TextInput from '@/components/UI_Elements/Form/TextInput';
+import SelectInput from '@/components/UI_Elements/Form/SelectInput';
 import Button from '@/components/UI_Elements/buttons/Button';
-import { signUp } from '../actions';
+import { useRouter } from 'next/navigation';
 
-// error messages for yup-password validation
-yup.setLocale({
-  string: {
-    minLowercase: 'Password must contain at least 1 lower case character.',
-    minUppercase: 'Password must contain at least 1 upper case character.',
-    minNumbers: 'Password must contain at least 1 number.',
-    minSymbols: 'Password must contain at least 1 special character.',
-    min: 'Password must be at least 8 characters.',
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  } as any,
-  // per docs- when using typescript, append `as any` to the end of this object to avoid type errors.
+const orgSchema = yup.object().shape({
+  'Company Name': yup.string().required('First Name is required.'),
+  Address: yup.string().required('First Name is required.'),
+  'Address Field 2': yup.string(),
+  City: yup.string().required(),
+  State: yup.string().required(),
+  Zip: yup.string().required(),
+  Country: yup.string().required(),
+  Telephone: yup.string().required(),
+  'Toll Free': yup.string(),
+  Fax: yup.string(),
+  'Docket Number Type': yup.string().required(),
+  'Docket Number': yup.string().required(),
+  'DOT ID#': yup.string(),
 });
 
-const newUserSchema = yup.object().shape({
-  firstName: yup.string().required('First Name is required.'),
-  lastName: yup.string().required('First Name is required.'),
-  email: yup
-    .string()
-    .email('Must use a valid email.')
-    .required('Email is required.'),
-  tel: yup.string().required('Phone Number is required.'),
-  password: yup.string().password().required('Password is required.'),
-});
+type NewOrg = yup.InferType<typeof orgSchema>;
 
-type NewUser = yup.InferType<typeof newUserSchema>;
+export default function Signup() {
+  const router = useRouter();
 
-export default function Signup({
-  searchParams,
-}: {
-  searchParams: { message: string };
-}) {
+  const { saveFormValues } = useContext(ModalContext);
+
   const {
-    register,
+    control,
     handleSubmit,
-    setError,
+    reset,
+    // setError,
     formState: { errors, isSubmitting },
-  } = useForm<NewUser>({
+  } = useForm<NewOrg>({
     defaultValues: {
-      firstName: '',
-      lastName: '',
-      email: '',
-      tel: '',
-      password: '',
+      'Company Name': '',
+      Address: '',
+      'Address Field 2': '',
+      City: '',
+      State: '',
+      Zip: '',
+      Country: '',
+      Telephone: '',
+      'Toll Free': '',
+      Fax: '',
+      'Docket Number Type': '',
+      'Docket Number': '',
+      'DOT ID#': '',
     },
-    resolver: yupResolver(newUserSchema),
+    resolver: yupResolver(orgSchema),
   });
 
-  const onSubmit = async (data: NewUser) => {
-    try {
-      console.log('signup data:', data);
-
-      await signUp(data);
-    } catch (error) {
-      console.log('Error submitting form:', error);
-      setError('root', { message: 'Error Submitting Form - Please try Again' });
-    }
+  const onSubmit = (data: NewOrg) => {
+    console.log('clicked', data);
+    saveFormValues(data);
+    reset(); // update form to default values
+    router.push('/signup/user'); // next step
   };
 
   return (
     <>
-      <p className="body2 dark:text-black mt-4">Please enter company details</p>
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
+        className="flex flex-col justify-between flex-grow"
       >
-        <div className="relative">
-          <input
-            {...register('firstName')}
-            type="text"
-            name="firstName"
-            id="firstName"
-            className="block px-2.5 pb-2.5 pt-4 w-full body2 dark:text-black bg-transparent rounded-lg border border-grey-400 appearance-none  focus:outline-none focus:ring-0 focus:border-primary peer"
-            placeholder=""
-            autoComplete="given-name"
-            required
-          />
+        <p className="mt-3.5 mb-5 body2 text-grey-800 text-center">
+          Enter details about your company
+        </p>
 
-          <label
-            htmlFor="firstName"
-            className="absolute body2 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:text-black px-2 peer-focus:px-2 peer-focus:text-primary  peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
-          >
-            First Name*
-          </label>
+        <div className="flex flex-col gap-4 md:flex-row">
+          <div className="w-full md:w-1/2">
+            <TextInput control={control} name="Company Name" required={true} />
+
+            <TextInput control={control} name="Toll Free" />
+          </div>
+          <div className="w-full md:w-1/2">
+            <TextInput control={control} name="Telephone" required={true} />
+
+            <TextInput control={control} name="Fax" />
+          </div>
         </div>
-        {errors.firstName && (
-          <p className="font-public font-normal text-text-sm mb-1 text-danger text-center">
-            {errors.firstName.message}
-          </p>
-        )}
 
-        <div className="relative">
-          <input
-            {...register('lastName')}
-            type="text"
-            name="lastName"
-            id="lastName"
-            className="block px-2.5 pb-2.5 pt-4 w-full body2 dark:text-black bg-transparent rounded-lg border border-grey-400 appearance-none  focus:outline-none focus:ring-0 focus:border-primary peer"
-            placeholder=""
-            autoComplete="family-name"
-            required
-          />
+        <TextInput control={control} name="Address" required={true} />
 
-          <label
-            htmlFor="lastName"
-            className="absolute body2 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:text-black px-2 peer-focus:px-2 peer-focus:text-primary  peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
-          >
-            Last Name*
-          </label>
+        <TextInput control={control} name="Address Field 2" />
+
+        {/* City, State, Zip, Country */}
+        <div className="flex flex-col gap-4 md:flex-row">
+          <div className="w-full md:w-1/4">
+            <TextInput control={control} name="City" required={true} />
+          </div>
+          <div className="w-full md:w-1/4">
+            <TextInput control={control} name="State" required={true} />
+          </div>
+          <div className="w-full md:w-1/4">
+            <TextInput control={control} name="Zip" required={true} />
+          </div>
+          <div className="w-full md:w-1/4">
+            <TextInput control={control} name="Country" required={true} />
+          </div>
         </div>
-        {errors.lastName && (
-          <p className="font-public font-normal text-text-sm mb-1 text-danger text-center">
-            {errors.lastName.message}
-          </p>
-        )}
 
-        <div className="relative ">
-          <input
-            {...register('email')}
-            type="text"
-            name="email"
-            id="email"
-            className="block px-2.5 pb-2.5 pt-4 w-full body2 dark:text-black bg-transparent rounded-lg border border-grey-400 appearance-none  focus:outline-none focus:ring-0 focus:border-primary peer"
-            placeholder=""
-            autoComplete="email"
-            required
-          />
-          <label
-            htmlFor="email"
-            className="absolute body2 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:text-black px-2 peer-focus:px-2 peer-focus:text-primary  peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
-          >
-            Email*
-          </label>
+        <div className="flex flex-col gap-4 md:flex-row">
+          <div className="w-full md:w-1/3">
+            <SelectInput
+              control={control}
+              name="Docket Number Type"
+              options={[{ MC: 'MC' }, { FF: 'FF' }]}
+            />
+          </div>
+          <div className="w-full md:w-1/3">
+            <TextInput control={control} name="Docket Number" />
+          </div>
+          <div className="w-full md:w-1/3">
+            <TextInput control={control} name="DOT ID#" />
+          </div>
         </div>
-        {errors.email && (
-          <p className="font-public font-normal text-text-sm mb-1 text-danger text-center">
-            {errors.email.message}
-          </p>
-        )}
-
-        <div className="relative">
-          <input
-            {...register('tel')}
-            type="text"
-            name="tel"
-            id="tel"
-            className="block px-2.5 pb-2.5 pt-4 w-full body2 dark:text-black bg-transparent rounded-lg border border-grey-400 appearance-none  focus:outline-none focus:ring-0 focus:border-primary peer"
-            placeholder=""
-            autoComplete="tel"
-            required
-          />
-          <label
-            htmlFor="tel"
-            className="absolute body2 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:text-black px-2 peer-focus:px-2 peer-focus:text-primary  peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
-          >
-            Phone Number*
-          </label>
-        </div>
-        {errors.tel && (
-          <p className="font-public font-normal text-text-sm mb-1 text-danger text-center">
-            {errors.tel.message}
-          </p>
-        )}
-
-        <div className="relative">
-          <input
-            {...register('password')}
-            type="password"
-            name="password"
-            id="password"
-            className="block px-2.5 pb-2.5 pt-4 w-full body2 dark:text-black bg-transparent rounded-lg border border-grey-400 appearance-none focus:outline-none focus:ring-0 focus:border-primary peer"
-            placeholder=""
-            autoComplete="new-password"
-            required
-          />
-          <label
-            htmlFor="password"
-            className="absolute body2 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:text-black px-2 peer-focus:px-2 peer-focus:text-primary  peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
-          >
-            Password*
-          </label>
-        </div>
-        {errors.password && (
-          <p className="font-public font-normal text-text-sm mb-1 text-danger text-center">
-            {errors.password.message}
-          </p>
-        )}
-
-        <Button type="submit">{isSubmitting ? 'Submitting' : 'Sign Up'}</Button>
 
         {errors.root && (
           <p className="font-public font-normal text-text-sm text-danger text-center mt-2">
@@ -202,11 +130,13 @@ export default function Signup({
           </p>
         )}
 
-        {searchParams?.message && (
-          <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
-            {searchParams.message}
-          </p>
-        )}
+        <div className="py-3.5 gap-2 border-t border-grey-300 flex justify-between bottom-0">
+          <div className="ml-auto">
+            <Button type="submit" disabled={isSubmitting}>
+              Next
+            </Button>
+          </div>
+        </div>
       </form>
     </>
   );
--- src/app/signup/layout.tsx
+++ src/app/signup/layout.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import { Logo } from '@/assets/logo';
 import Link from 'next/link';
+import { ContextProvider } from '@/Context/modalContext';
 
 // TODO: change width and height to fit on screen
 
@@ -9,27 +12,25 @@ export default function SignUpLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="border rounded-lg border-grey-300 p-10 w-full max-w-md">
-        <header className="flex flex-col justify-between items-center mb-3">
-          {Logo}
-          <h1 className="text-title-md dark:text-black mt-6">
-            Sign up to continue
-          </h1>
-        </header>
-        {children}
-        <div>
-          <p className="inline-block body2 dark:text-black mt-3 mr-3">
-            Already have an account?
-          </p>
-          <Link
-            className="text-primary hover:text-primary-dark justify-center font-public font-bold w-auto h-auto disabled:text-grey-500 disabled:pointer-events-none text-button-lg my-2"
-            href={'/login'}
-          >
-            Log In
-          </Link>
+    <ContextProvider>
+      <div className="min-h-screen bg-grey-100 flex items-center justify-center">
+        <div className=" bg-white border rounded-lg border-grey-300 p-10 mx-auto w-full max-w-280">
+          <header className="flex flex-col justify-between items-center mb-3">
+            {Logo}
+            <h2 className="text-title-md mt-6">Sign Up</h2>
+          </header>
+          {children}
+          <div className="relative block -mt-12 w-56 mx-auto">
+            <p className="inline-block body2 mr-3">Already have an account?</p>
+            <Link
+              className="text-primary hover:text-primary-dark justify-center font-public font-bold w-auto h-auto disabled:text-grey-500 disabled:pointer-events-none text-button-lg my-2"
+              href={'/login'}
+            >
+              Log In
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </ContextProvider>
   );
 }
