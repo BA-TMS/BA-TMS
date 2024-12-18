97974af0f8bf4c5a66b92306a2db196e96dbc8a8
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-12-18 11:46:30 -0800

    truck types

---
+++ src/types/truckTypes.ts
@@ -0,0 +1,8 @@
+export interface TruckFormData {
+  'Truck Number': string;
+  'License Plate': string;
+  'Truck Type': string;
+  'Plate Expiry': string;
+  'Inspection Expiry': string;
+  'IFTA Licensed': boolean | undefined;
+}
