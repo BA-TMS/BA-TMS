9fb7d46fa2491214fbb263d15056284cba707e84
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-12-18 11:21:41 -0800

    consignee types

---
+++ src/types/consigneeTypes.ts
@@ -0,0 +1,13 @@
+export interface ConsigneeFormData {
+  'Consignee Name': string;
+  Address: string;
+  'Address Line 2'?: string;
+  City: string;
+  State: string;
+  Country: string;
+  Zip: string;
+  'Country Code': string;
+  'Phone Number': string;
+  'Contact Name'?: string;
+  Notes?: string;
+}
