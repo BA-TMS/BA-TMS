d0dc56284104eda68b288152230b05dd803bface
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-12-19 12:01:05 -0800

    initial page setup

--- src/app/(authenticated)/settings/password/page.tsx
+++ src/app/(authenticated)/settings/password/page.tsx
@@ -1,68 +1,116 @@
 'use client';
-import React, { useState } from 'react';
+
+import { useState } from 'react';
+import * as yup from 'yup';
+import YupPassword from 'yup-password';
+YupPassword(yup);
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
+import TextInput from '@/components/UI_Elements/Form/TextInput';
+import Button from '@/components/UI_Elements/Buttons/Button';
+
+// error messages for yup-password validation- passwords need to match
+yup.setLocale({
+  string: {
+    minLowercase: 'Password must contain at least 1 lower case character.',
+    minUppercase: 'Password must contain at least 1 upper case character.',
+    minNumbers: 'Password must contain at least 1 number.',
+    minSymbols: 'Password must contain at least 1 special character.',
+    min: 'Password must be at least 8 characters.',
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } as any,
+  // per docs- when using typescript, append `as any` to the end of this object to avoid type errors.
+});
+
+const passwordSchema = yup.object().shape({
+  Password: yup.string().password().required('Password is required.'),
+  'Confirm Password': yup
+    .string()
+    .password()
+    .required('Password is required.')
+    .oneOf([yup.ref('Password')], 'Passwords must match.'),
+});
+
+type Password = yup.InferType<typeof passwordSchema>;
 
 const PasswordReset = () => {
-  const [oldPassword, setOldPassword] = useState('');
-  const [newPassword, setNewPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
 
-  // Show/Hide Password variables
-  const [showOldPassword, setShowOldPassword] = useState<boolean>(false);
-  const [showNewPassword, setShowNewPassword] = useState<boolean>(false);
-  const [showConfirmPassword, setShowConfirmPassword] =
-    useState<boolean>(false);
+  const {
+    control,
+    handleSubmit,
+    // reset,
+    // setError,
+    formState: { errors },
+  } = useForm<Password>({
+    defaultValues: {
+      Password: '',
+      'Confirm Password': '',
+    },
+    resolver: yupResolver(passwordSchema),
+  });
 
-  const handleOldPassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setOldPassword(e.target.value);
+  const onSubmit = (data: Password) => {
+    setIsSubmitting(true);
+    console.log('DATA', data);
+
+    // try {
+    //   setPassword(data['Password'], refreshToken as string); // it should be a string at this point
+    // } catch (error) {
+    //   setError('root', { message: `${error}` });
+    // }
+    setTimeout(() => {
+      setIsSubmitting(false);
+    }, 1000);
   };
-
-  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setNewPassword(e.target.value);
-  };
-
-  const handleConfirmPasswordChange = (
-    e: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setConfirmPassword(e.target.value);
-  };
-
-  const handleResetPassword = () => {
-    // Add logic to reset password
-    if (newPassword === confirmPassword) {
-      // Call API to reset password
-      console.log('Password reset successful');
-    } else {
-      console.error('Passwords do not match');
-    }
-  };
-
-  // Show/Hide Password
 
   return (
-    <>
-      <h2>Reset Password Page</h2>
-      <input
-        type={showOldPassword ? 'text' : 'password'}
-        id="oldPassword"
-        value={oldPassword}
-        onChange={handleOldPassword}
-      />
+    <div className="bg-grey-100 dark:bg-grey-800 flex flex-col items-center justify-center">
+      <div className="flex flex-col bg-white border rounded-lg border-grey-300 px-4.5 pt-8 w-4/6 h-5/6">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col h-full justify-between"
+        >
+          <p className="mt-3.5 mb-5 body2 text-grey-800 text-center">
+            Change Password
+          </p>
 
-      <input
-        type={showNewPassword ? 'text' : 'password'}
-        id="newPassword"
-        value={newPassword}
-        onChange={handlePasswordChange}
-      />
+          <div className="flex flex-col items-center w-full">
+            <div className="w-2/3">
+              <TextInput
+                control={control}
+                name="Password"
+                required={true}
+                type="password"
+              />
 
-      <input
-        type={showConfirmPassword ? 'text' : 'password'}
-        id="confirmPassword"
-        value={confirmPassword}
-        onChange={handleConfirmPasswordChange}
-      />
-      <button onClick={handleResetPassword}>Reset Password</button>
-    </>
+              <TextInput
+                control={control}
+                name="Confirm Password"
+                required={true}
+                type="password"
+              />
+            </div>
+          </div>
+
+          <div className="h-10 text-center">
+            {errors.root && (
+              <p className="font-public font-normal text-text-sm text-error mb-2">
+                {errors.root.message}
+              </p>
+            )}
+          </div>
+
+          <div className="py-4 gap-2 border-t border-grey-300 bg-white flex justify-end sticky bottom-0 z-10">
+            <div className="flex justify-end gap-2">
+              <Button type="submit" disabled={isSubmitting}>
+                Next
+              </Button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 };
 
