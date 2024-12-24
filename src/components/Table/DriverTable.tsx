65832754b5dd0cb20643d855a7b77dbdd8d1f409
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-12-23 16:06:25 -0800

    table search

--- src/components/Table/DriverTable.tsx
+++ src/components/Table/DriverTable.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '@/store/store';
-// import { ModalContext } from '@/context/modalContext';
+import { ModalContext } from '@/context/modalContext';
 import Table from '../UI_Elements/Table/Table';
 import TableSkeleton from '../UI_Elements/Table/TableSkeleton';
 import TableHeaderBlank from '../UI_Elements/Table/TableHeaderBlank';
@@ -11,8 +11,8 @@ import { TableSearch } from '../UI_Elements/Table/TableSearch';
 import Button from '../UI_Elements/Buttons/Button';
 import Link from 'next/link';
 // import { useRouter } from 'next/navigation';
-import { getDrivers } from '@/lib/dbActions';
 import { fetchDrivers } from '@/store/slices/driverSlice';
+import { DriverFormData } from '@/types/driverTypes';
 
 type Driver = {
   name: string;
@@ -44,14 +44,61 @@ export default function Drivers() {
     // error,
   } = useSelector((state: RootState) => state.drivers);
 
+  const { saveFormValues } = useContext(ModalContext);
+
+  // search
+  function handleSearch(
+    drivers: DriverFormData[],
+    value: string,
+    status: string
+  ) {
+    // status to uppercase
+    const driverStatus = status?.toUpperCase();
+
+    // Filter by status (if it's "Active" or "Inactive")
+    let filteredDrivers = drivers;
+
+    if (driverStatus === 'ACTIVE' || driverStatus === 'INACTIVE') {
+      filteredDrivers = drivers.filter(
+        (driver) => driver.status === driverStatus
+      );
+    }
+
+    // If no search value, return the filtered list by status
+    if (!value) {
+      return filteredDrivers;
+    }
+
+    // search across all fields with the given value
+    if (status === 'All') {
+      return filteredDrivers.filter((driver) =>
+        Object.values(driver).some((carrierField) =>
+          carrierField?.toString().toLowerCase().includes(value.toLowerCase())
+        )
+      );
+    }
+
+    // If status is specific (like "Active"), apply search value filtering
+    return filteredDrivers.filter((driver) =>
+      Object.values(driver).some((driverField) =>
+        driverField?.toString().toLowerCase().includes(value.toLowerCase())
+      )
+    );
+  }
+
+  // update specific field to search
+  function updateField(field: string) {
+    setSearchField(field);
+  }
+
   useEffect(() => {
     dispatch(fetchDrivers());
   }, [dispatch]);
 
-  // Update filtered drivers when carrier or searchValue changes
+  // Update filtered drivers when driver or searchValue changes
   useEffect(() => {
-    const updatedDrivers = [...drivers];
-    // updatedDrivers = handleSearch(updatedDrivers, searchValue, searchField);
+    let updatedDrivers = [...drivers];
+    updatedDrivers = handleSearch(updatedDrivers, searchValue, searchField);
     setFilteredValue(updatedDrivers);
   }, [drivers, searchValue, searchField]);
 
@@ -70,7 +117,7 @@ export default function Drivers() {
         dropdownLabel="Status"
         dropdownOptions={['Active', 'Inactive', 'All']}
         search={setSearchValue}
-        updateField={() => null}
+        updateField={updateField}
       />
 
       {status === 'loading' ? (
