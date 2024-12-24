8f8f267044d0ea8546ff093a77251135ade27d2a
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-12-24 12:11:38 -0800

    update- driver types

--- src/types/driverTypes.ts
+++ src/types/driverTypes.ts
@@ -1,3 +1,5 @@
+import { Load } from '@prisma/client';
+
 export interface DriverFormData {
   'Driver Name': string;
   'Country Code': string;
@@ -5,4 +7,29 @@ export interface DriverFormData {
   'License Number': string;
   Employer: string;
   Notes?: string | null;
+}
+
+export interface DriverData {
+  id: string;
+  createdAt: Date | string;
+  updatedAt: Date | string;
+  status: string; // "ACTIVE" | "INACTIVE" ??
+  name: string;
+  telephone: string;
+  email: string | null;
+  country: string;
+  state: string;
+  city: string;
+  zip: string | null;
+  license: string | null;
+  notes: string | null;
+
+  // are drivers linked with carriers always/ sometimes/ never?
+  employerId: string | null;
+  employer: { carrierName: string } | null;
+
+  loads?: Load[]; // are loads optional?
+
+  orgId: string;
+  organization: { orgName: string };
 }
