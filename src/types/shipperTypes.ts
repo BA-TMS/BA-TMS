04a3b6becbc98dadd1d02827df768af88a7b5cfc
Lane Hamilton <aleyna.hamilton@gmail.com>
2025-02-10 12:30:17 -0800

    ShipperData types

--- src/types/shipperTypes.ts
+++ src/types/shipperTypes.ts
@@ -1,3 +1,36 @@
+export interface ShipperData {
+  id: string;
+  createdAt: Date | string;
+  updatedAt: Date | string;
+  status: string;
+
+  name: string;
+  address: string | null;
+  addressField2: string | null;
+  addressField3: string | null;
+  city: string;
+  state: string;
+  postCode: string;
+  postCountry: string;
+
+  contactName: string | null;
+  contactEmail: string | null;
+  telephone: string | null;
+  tollFree: string | null;
+
+  shippingHours: string | null;
+  appointments: string | null;
+  intersections: string | null;
+
+  notes: string | null;
+
+  orgId: string;
+  organization: { orgName: string };
+
+  consigneeId: string | null;
+  consignee: { name: string }; // check this
+}
+
 export interface ShipperFormData {
   'Shipper Name': string;
   Address: string;
