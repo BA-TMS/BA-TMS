80ca8d2b60959e360c6123abdd27aaa3237a67e8
Lane Hamilton <aleyna.hamilton@gmail.com>
2025-02-12 14:02:09 -0800

    view shipper pages

--- src/app/(authenticated)/shippers/@modal/(.)view/page.tsx
+++ src/app/(authenticated)/shippers/@modal/(.)view/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from 'next/navigation';
 
-export default function ViewFactor() {
-  return redirect('/factors');
+export default function ViewShipper() {
+  return redirect('/shippers');
 }
--- src/app/(authenticated)/shippers/view/[id]/page.tsx
+++ src/app/(authenticated)/shippers/view/[id]/page.tsx
@@ -4,26 +4,28 @@ import { useSelector } from 'react-redux';
 import { usePathname } from 'next/navigation';
 import FullPageFormContainer from '@/components/UI_Elements/Form/FullPageContainer';
 import { RootState } from '@/store/store';
-import { FactorData } from '@/types/factorTypes';
-import ViewFactoringCo from '@/components/Forms/Factor/ViewFactoringCo';
+import { ShipperData } from '@/types/shipperTypes';
+import ViewShipperForm from '@/components/Forms/Shipper/ViewShipper';
 
 // page uses dynamic routing as we don't know what the id is
 // takes in the id to find the entry
 
-export default function ViewFactor() {
+export default function ViewShipper() {
   const pathname = usePathname();
 
   // slice the pathname to get the id
-  const factorId = pathname.split('/view/')[1];
+  const shipperId = pathname.split('/view/')[1];
 
   // use the id to pull from redux
-  const factor = useSelector((state: RootState) =>
-    state.factors.items.find((factor: FactorData) => factor.id === factorId)
+  const shipper = useSelector((state: RootState) =>
+    state.shippers.items.find(
+      (shipper: ShipperData) => shipper.id === shipperId
+    )
   );
 
   return (
-    <FullPageFormContainer title={'View Factoring Company'}>
-      <ViewFactoringCo data={factor} />
+    <FullPageFormContainer title={'View Shipper'}>
+      <ViewShipperForm data={shipper} />
     </FullPageFormContainer>
   );
 }
