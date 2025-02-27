11a5a14dd569db22795754b0afed3a446387b44b
Lane Hamilton <aleyna.hamilton@gmail.com>
2025-02-27 10:13:42 -0800

    create loadActions.ts

---
+++ src/lib/actions/loadActions.ts
@@ -0,0 +1,110 @@
+'use server';
+
+import prisma from '@util/prisma/client';
+import { getOrganization } from '@/lib/dbActions';
+import { LoadFormData } from '@/types/loadTypes';
+
+const LOAD_RELATIONS = {
+  carrier: { select: { carrierName: true } },
+  driver: { select: { name: true } },
+  customer: { select: { companyName: true } },
+  shipper: { select: { name: true } },
+  consignee: { select: { name: true } },
+};
+
+export async function getLoad(id: string) {
+  const load = await prisma.load.findUnique({
+    where: {
+      id: id,
+    },
+  });
+  return load;
+}
+
+export async function getLoads(organization: string) {
+  const loads = await prisma.load.findMany({
+    where: {
+      organization: {
+        orgName: organization,
+      },
+    },
+    orderBy: [
+      {
+        loadNum: 'asc',
+      },
+    ],
+    include: LOAD_RELATIONS,
+  });
+  return loads;
+}
+
+export async function addLoad({ load }: { load: LoadFormData }) {
+  // do not change to dispatched without a carrier
+  if (load['Status'] === 'DISPATCHED' && !load['Carrier']) {
+    throw new Error(
+      'Cannot create a load with status of "Dispatched" without a carrier.'
+    );
+  }
+
+  const resp = await prisma.load.create({
+    data: {
+      orgId: load['Owner'],
+      loadNum: load['Load Number'],
+      payOrderNum: load['Pay Order Number'],
+      carrierId: load['Carrier'],
+      driverId: load['Driver'],
+      customerId: load['Customer'],
+      originId: load['Shipper'],
+      destId: load['Consignee'],
+      status: load['Status'],
+      shipDate: load['Ship Date'],
+      deliveryDate: load['Received Date'],
+    },
+    include: LOAD_RELATIONS,
+  });
+  return resp;
+}
+
+export async function updateLoad(
+  id: string,
+  { formData }: { formData: Partial<LoadFormData> }
+) {
+  // do not change to dispatched without a carrier
+  if (formData['Status'] === 'DISPATCHED' && !formData['Carrier']) {
+    throw new Error(
+      'Cannot update a load status to "Dispatched" without a carrier.'
+    );
+  }
+  // map to convert formData keys to database keys
+  const mapLoadData = (load: Partial<LoadFormData>) => {
+    if (!load) {
+      throw new Error('Load data is undefined or null');
+    }
+
+    return {
+      orgId: load['Owner'],
+      loadNum: load['Load Number'],
+      payOrderNum: load['Pay Order Number'],
+      carrierId: load['Carrier'] ? load['Carrier'] : null,
+      driverId: load['Driver'] ? load['Driver'] : null,
+      customerId: load['Customer'],
+      originId: load['Shipper'] ? load['Shipper'] : null,
+      destId: load['Consignee'] ? load['Consignee'] : null,
+      status: load['Status'],
+      shipDate: load['Ship Date'],
+      deliveryDate: load['Received Date'],
+    };
+  };
+
+  const mappedLoad = mapLoadData(formData);
+
+  const resp = await prisma.load.update({
+    where: { id },
+    data: {
+      ...mappedLoad,
+    },
+    include: LOAD_RELATIONS,
+  });
+
+  return resp;
+}
--- src/lib/dbActions.ts
+++ src/lib/dbActions.ts
@@ -3,7 +3,6 @@
 
 import prisma from '@util/prisma/client';
 import { PrismaClient, DriverType } from '@prisma/client';
-import { LoadFormData } from '@/types/loadTypes';
 import { DriverFormData } from '@/types/driverTypes';
 import { BilleeFormData } from '@/types/billeeTypes';
 import { TrailerFormData } from '@/types/trailerTypes';
@@ -11,14 +10,6 @@ import { TruckFormData } from '@/types/truckTypes';
 import { AccountPreferences } from '@/types/accountTypes';
 
 // This file contains different server actions for interracting with the database via Prisma client
-
-const LOAD_RELATIONS = {
-  carrier: { select: { carrierName: true } },
-  driver: { select: { name: true } },
-  customer: { select: { companyName: true } },
-  shipper: { select: { name: true } },
-  consignee: { select: { name: true } },
-};
 
 const DRIVER_RELATIONS = {
   organization: { select: { orgName: true } },
@@ -68,20 +59,6 @@ export async function getDrivers(organization: string) {
     include: DRIVER_RELATIONS,
   });
   return drivers;
-}
-
-export async function getLoad(id: string) {
-  const load = await prisma.load.findUnique({
-    where: {
-      id: id,
-    },
-  });
-  return load;
-}
-
-export async function getLoads() {
-  const loads = await getter(prisma.load, LOAD_RELATIONS);
-  return loads;
 }
 
 export async function getOrganization(orgName: string) {
@@ -217,33 +194,6 @@ export async function addDriver({ driver }: { driver: DriverFormData }) {
   }
 }
 
-export async function addLoad({ load }: { load: LoadFormData }) {
-  // do not change to dispatched without a carrier
-  if (load['Status'] === 'DISPATCHED' && !load['Carrier']) {
-    throw new Error(
-      'Cannot create a load with status of "Dispatched" without a carrier.'
-    );
-  }
-
-  const resp = await prisma.load.create({
-    data: {
-      ownerId: load['Owner'],
-      loadNum: load['Load Number'],
-      payOrderNum: load['Pay Order Number'],
-      carrierId: load['Carrier'],
-      driverId: load['Driver'],
-      customerId: load['Customer'],
-      originId: load['Shipper'],
-      destId: load['Consignee'],
-      status: load['Status'],
-      shipDate: load['Ship Date'],
-      deliveryDate: load['Received Date'],
-    },
-    include: LOAD_RELATIONS,
-  });
-  return resp;
-}
-
 export async function addThirdParty({ billee }: { billee: BilleeFormData }) {
   const resp = await prisma.billee.create({
     data: {
@@ -365,50 +315,6 @@ export async function updateDriver(
     },
     include: DRIVER_RELATIONS,
   });
-  return resp;
-}
-
-export async function updateLoad(
-  id: string,
-  { formData }: { formData: Partial<LoadFormData> }
-) {
-  // do not change to dispatched without a carrier
-  if (formData['Status'] === 'DISPATCHED' && !formData['Carrier']) {
-    throw new Error(
-      'Cannot update a load status to "Dispatched" without a carrier.'
-    );
-  }
-  // map to convert formData keys to database keys
-  const mapLoadData = (load: Partial<LoadFormData>) => {
-    if (!load) {
-      throw new Error('Load data is undefined or null');
-    }
-
-    return {
-      ownerId: load['Owner'],
-      loadNum: load['Load Number'],
-      payOrderNum: load['Pay Order Number'],
-      carrierId: load['Carrier'] ? load['Carrier'] : null,
-      driverId: load['Driver'] ? load['Driver'] : null,
-      customerId: load['Customer'],
-      originId: load['Shipper'] ? load['Shipper'] : null,
-      destId: load['Consignee'] ? load['Consignee'] : null,
-      status: load['Status'],
-      shipDate: load['Ship Date'],
-      deliveryDate: load['Received Date'],
-    };
-  };
-
-  const mappedLoad = mapLoadData(formData);
-
-  const resp = await prisma.load.update({
-    where: { id },
-    data: {
-      ...mappedLoad,
-    },
-    include: LOAD_RELATIONS,
-  });
-
   return resp;
 }
 
