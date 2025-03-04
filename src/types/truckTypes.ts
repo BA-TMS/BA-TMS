4ffb44552e2f54d14150ca2831b6111060ca3099
Lane Hamilton <aleyna.hamilton@gmail.com>
2025-03-04 11:05:49 -0800

    truck types

--- src/types/truckTypes.ts
+++ src/types/truckTypes.ts
@@ -7,4 +7,30 @@ export interface TruckFormData {
   'IFTA Licensed': boolean | undefined;
 }
 
-export interface TruckData {}
+export interface TruckData {
+  id: string;
+  createdAt: Date | string;
+  updatedAt: Date | string;
+  status: string;
+
+  truckNum: string;
+  licensePlate: string | null;
+  plateExpiry: Date | null;
+  inspectionExpiry: Date | null;
+  type: string | null;
+  ownership: string | null;
+
+  orgId: string;
+  organization: { orgName: string };
+
+  mileage: string | null;
+  axels: string | null;
+  fuelType: string | null;
+  year: string | null;
+  startDate: Date | null;
+  deactivationDate: Date | null;
+  registeredState: string | null;
+  weight: string | null;
+  vin: string | null;
+  dotExpiry: Date | null;
+}
