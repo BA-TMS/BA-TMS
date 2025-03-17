cea48b622de91880e5a365dfad75604ed174b69e
Lane Hamilton <aleyna.hamilton@gmail.com>
2025-03-17 14:27:38 -0700

    View form

---
+++ src/components/Forms/OtherNumbers/ViewOtherNumbers.tsx
@@ -0,0 +1,98 @@
+'use client';
+
+import { useContext } from 'react';
+import { ModalContext } from '@/context/modalContext';
+import { useRouter } from 'next/navigation';
+import { NumData } from '@/types/otherNumTypes';
+import Button from '@/components/UI_Elements/Buttons/Button';
+import DataDisplay from '@/components/UI_Elements/Display/DataDisplay';
+import CheckBox from '@/components/UI_Elements/Form/CheckBox';
+
+// this component displays information about a specific other number
+
+interface ViewNumProps {
+  data: NumData | undefined;
+}
+
+export const ViewOtherNumbers = ({ data }: ViewNumProps) => {
+  const router = useRouter();
+
+  const { saveFormValues } = useContext(ModalContext);
+
+  if (!data) {
+    return (
+      <div className="flex flex-col h-full">
+        <div className="py-5 flex-grow">
+          <p className="body2 text-error-dark text-center">
+            Oops! Something went wrong- Could not find Other Number.
+          </p>
+        </div>
+        <div className="py-3.5 gap-2 border-t border-grey-300 dark:border-grey-700 flex justify-end bg-white dark:bg-grey-900 z-10">
+          <Button
+            type="button"
+            onClick={() => {
+              router.back();
+            }}
+          >
+            Back
+          </Button>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col h-full">
+      <div className="flex flex-col justify-between flex-grow">
+        <p className="my-3.5 body2 text-grey-800 dark:text-white">
+          View Other Number
+        </p>
+
+        <div className="flex-grow">
+          <div className="flex flex-col gap-4 md:flex-row">
+            <div className="w-full md:w-1/2">
+              <DataDisplay title="Other Number" text={data['name']} />
+            </div>
+
+            <div className="w-full md:w-1/2">
+              <DataDisplay title="Status" text={data['status']} />
+            </div>
+          </div>
+
+          <CheckBox
+            id={'dispatch'}
+            onChange={null}
+            label="Added to Dispatch Board"
+            checked={data.dispatch}
+          />
+        </div>
+
+        <div className="py-3.5 gap-2 border-t border-grey-300 dark:border-grey-700 flex justify-between sticky bottom-0 bg-white dark:bg-grey-900 z-10">
+          <Button
+            type="button"
+            variant="outline"
+            intent="default"
+            onClick={() => {
+              // send data to context
+              saveFormValues(data);
+              router.push('/other-numbers/update-numbers/details');
+            }}
+          >
+            Edit
+          </Button>
+
+          <Button
+            type="button"
+            onClick={() => {
+              router.back();
+            }}
+          >
+            Ok
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ViewOtherNumbers;
