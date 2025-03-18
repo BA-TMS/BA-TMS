4e372c7ce6bacb5b38ff864a93440ac620bd30dd
Lane Hamilton <aleyna.hamilton@gmail.com>
2025-03-18 12:10:44 -0700

    icon display component

---
+++ src/components/UI_Elements/Display/IconDisplay.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+'use client';
+
+import { SvgIconComponent } from '@mui/icons-material';
+
+// this component displays information
+
+interface DisplayProps {
+  title: string;
+
+  Icon: SvgIconComponent; // accepts an MUI icon as a prop
+}
+
+const IconDisplay = ({ title, Icon }: DisplayProps) => {
+  return (
+    <div className="flex items-center ml-2 my-1.5 gap-2">
+      <Icon />
+      <p className="subtitle2 px-2 title-grey-800 dark:text-white bg-transparent">
+        {title}
+      </p>
+    </div>
+  );
+};
+
+export default IconDisplay;
