d3d24764ba36d7e47904eb3364bae1b0a7dffefc
Ardhendu Pramanik <ardhendu@a2zport.com>
2025-04-02 20:41:14 +0530

    fix: some unnessary comments has been removed

--- src/app/(authenticated)/customers/layout.tsx
+++ src/app/(authenticated)/customers/layout.tsx
@@ -2,9 +2,6 @@
 
 import { ContextProvider } from '../../../context/modalContext';
 
-// slots are passed as props to the shared parent layout
-// slots render parallel alongside children prop
-
 export default function CustomerTemplage({
   children,
   modal,
