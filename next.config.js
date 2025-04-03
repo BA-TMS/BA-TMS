71b263606e2fee5ef8d23b814e9b5c9447ac66ad
Ardhendu Pramanik <ardhendu@a2zport.com>
2025-04-03 11:30:33 +0530

    fix: next config file

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
-import path from 'path';
+
 
 const nextConfig = {
-  webpack: (config) => {
-    config.resolve.modules.push(path.resolve('./src'));
-    return config;
-  },
+ 
   async redirects() {
     return [
       {
